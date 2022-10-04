3ef3a6149cca0a615eda62a48710a45d177a5839
Kristian Kjerstad <69512295+KristianKjerstad@users.noreply.github.com>
2022-10-04 14:43:04 +0200

    Feat/option to specify url for dmss api (#6)
    * feat: Add option to specify url for DMSS, and add an example of usage

--- packages/dmt-core/src/services/api/DmssAPI.ts
+++ packages/dmt-core/src/services/api/DmssAPI.ts
@@ -1,9 +1,13 @@
 import { Configuration, DefaultApi } from './configs/gen'
 
 export class DmssAPI extends DefaultApi {
-  constructor(token: string) {
+  constructor(token: string, dmssBasePath?: string) {
+    /*
+      dmssBasePath is an optional parameter where you can specify the url for DMSS. Example: http://localhost:8000
+      (Note: a forward slash should not be included at the end of the url)
+    */
     const DMSSConfiguration = new Configuration({
-      basePath: '/api/dmss',
+      basePath: dmssBasePath || '/api/dmss',
       accessToken: token,
     })
     super(DMSSConfiguration)
