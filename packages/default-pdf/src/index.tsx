d79cbb4bfc67e7b806ff0f2f62e391fe632881ff
KristianKjerstad <kristian.kjerstad@bouvet.no>
2022-11-16 09:23:01 +0100

    chore: update dm-core version in default-pdf package

--- packages/default-pdf/src/index.tsx
+++ packages/default-pdf/src/index.tsx
@@ -1,26 +1,23 @@
 import * as React from 'react'
 import { ViewerPDFPlugin } from './PDFViewer'
 
-import {
-  EDmtPluginType,
-  IDmtUIPlugin,
-  Loading,
-} from '@development-framework/dm-core'
+import { EPluginType, IUIPlugin, Loading } from '@development-framework/dm-core'
 import { useDocument } from '@development-framework/dm-core'
 
-const PluginComponent = (props: IDmtUIPlugin) => {
-  const { documentId, dataSourceId } = props
-  const [document, loading] = useDocument(dataSourceId, documentId, 999)
+const PluginComponent = (props: IUIPlugin) => {
+  const { idReference } = props
+  const [document, loading] = useDocument(idReference, 999)
+  const dataSource = idReference.split('/')[0]
 
   if (loading) return <Loading />
 
-  return <ViewerPDFPlugin document={document} dataSourceId={dataSourceId} />
+  return <ViewerPDFPlugin document={document} dataSourceId={dataSource} />
 }
 
 export const plugins: any = [
   {
     pluginName: 'default-pdf',
-    pluginType: EDmtPluginType.UI,
+    pluginType: EPluginType.UI,
     component: PluginComponent,
   },
 ]
