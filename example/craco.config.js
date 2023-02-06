ad3b7b61c7978592829c95289e494c34c132e6ee
Eirik Ola Aksnes <eoaksnes@gmail.com>
2023-02-06 18:51:24 +0100

    build: enable hot reloading for packages

---
+++ example/craco.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
+
+module.exports = {
+  webpack: {
+    configure: (config) => {
+      // Remove guard against importing modules outside of \`src\`.
+      // Needed for workspace projects.
+      config.resolve.plugins = config.resolve.plugins.filter(
+        (plugin) => !(plugin instanceof ModuleScopePlugin)
+      )
+
+      // Add support for importing workspace projects.
+      config.resolve.plugins.push(
+        new TsConfigPathsPlugin({
+          configFile: path.resolve(__dirname, 'tsconfig.json'),
+          extensions: ['.ts', '.tsx', '.js', '.jsx'],
+          mainFields: ['module', 'main'],
+        })
+      )
+      const oneOfRule = config.module.rules.find((rule) => rule.oneOf)
+      // Replace include option for babel loader with exclude
+      // so babel will handle workspace projects as well.
+      oneOfRule.oneOf.forEach((r) => {
+        if (r.loader && r.loader.indexOf('babel') !== -1) {
+          r.exclude = /.yarn/
+          delete r.include
+        }
+      })
+
+      return config
+    },
+  },
+}
