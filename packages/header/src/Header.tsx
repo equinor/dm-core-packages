fcd25350f5c6592df9d244f9c477edf70f6742b6
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-02-06 14:53:02 +0100

    refactor: in header plugin, split up dialog into new components

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -1,21 +1,14 @@
-import {
-  Button,
-  Dialog,
-  DmssAPI,
-  IUIPlugin,
-  Loading,
-  useDocument,
-} from '@development-framework/dm-core'
-import React, { useContext, useState } from 'react'
+import { IUIPlugin, Loading, useDocument } from '@development-framework/dm-core'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Icon, Radio, TopBar } from '@equinor/eds-core-react'
+import { Icon, TopBar } from '@equinor/eds-core-react'
 
 // @ts-ignore
 import { NotificationManager } from 'react-notifications'
-import { AxiosResponse } from 'axios'
-import { AuthContext } from 'react-oauth2-code-pkce'
-import { useLocalStorage } from './useLocalStorage'
 import { account_circle, grid_on, info_circle } from '@equinor/eds-icons'
+import { UserInfoDialog } from './components/UserInfoDialog'
+import { AboutDialog } from './components/AboutDialog'
+import { TApplication } from './types'
 
 const Icons = styled.div`
   display: flex;
@@ -66,50 +59,20 @@ const ClickableIcon = styled.div`
   }
 `
 
-const UserInfoLabel = styled.b`
-  margin-left: 5px;
-`
-
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-`
-
-const UnstyledList = styled.ul`
-  margin: 0;
-  padding: 0;
-  list-style-type: none;
-  display: inline-flex;
-`
-
 type THeaderPluginConfig = {
   hideUserInfo: boolean
   hideAbout: boolean
-}
-
-type TApplication = {
-  name: string
-  dataSources: string[]
-  label: string
-  description: string
-  roles: string[]
-  adminRole: string
 }
 
 export default (props: IUIPlugin): JSX.Element => {
   const { idReference, config: passedConfig } = props
   const config: THeaderPluginConfig = passedConfig
   const [entity, isLoading] = useDocument<TApplication>(idReference)
-  const { tokenData, token, logOut } = useContext(AuthContext)
+
   const [aboutOpen, setAboutOpen] = useState(false)
   const [visibleUserInfo, setVisibleUserInfo] = useState<boolean>(false)
   const [appSelectorOpen, setAppSelectorOpen] = useState<boolean>(false)
   const [apiKey, setAPIKey] = useState<string | null>(null)
-  const dmssApi = new DmssAPI(token)
-  const [checked, updateChecked] = useLocalStorage<string | null>(
-    'impersonateRoles',
-    null
-  )
 
   if (isLoading || !entity) {
     return <Loading />
@@ -152,82 +115,16 @@ export default (props: IUIPlugin): JSX.Element => {
         </Icons>
       </TopBar.Actions>
       <TopBar.CustomContent>
-        <Dialog
+        <AboutDialog
           isOpen={aboutOpen}
-          closeScrim={() => setAboutOpen(false)}
-          header={`About ${entity.label}`}
-          width={'40vw'}
-        >
-          <p style={{ padding: '0 15px' }}>{entity.description}</p>
-        </Dialog>
-        <Dialog
+          setIsOpen={setAboutOpen}
+          applicationEntity={entity}
+        />
+        <UserInfoDialog
           isOpen={visibleUserInfo}
-          header={'User info'}
-          closeScrim={() => setVisibleUserInfo(false)}
-          width={'720px'}
-        >
-          <div style={{ margin: '20px' }}>
-            <Row>
-              Name:<UserInfoLabel>{tokenData?.name}</UserInfoLabel>
-            </Row>
-            <Row>
-              Username:
-              <UserInfoLabel>{tokenData?.preferred_username}</UserInfoLabel>
-            </Row>
-            <Row>
-              Roles:{' '}
-              <UserInfoLabel>{JSON.stringify(tokenData?.roles)}</UserInfoLabel>
-            </Row>
-            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-              <Button
-                onClick={() => {
-                  navigator.clipboard.writeText(token)
-                  NotificationManager.success('Copied token to clipboard')
-                }}
-              >
-                Copy token to clipboard
-              </Button>
-              <Button
-                onClick={() =>
-                  dmssApi
-                    .tokenCreate()
-                    .then((response: AxiosResponse<string>) =>
-                      setAPIKey(response.data)
-                    )
-                    .catch((error: any) => {
-                      console.error(error)
-                      NotificationManager.error(
-                        'Failed to create personal access token'
-                      )
-                    })
-                }
-              >
-                Create API-Key
-              </Button>
-              <Button onClick={() => logOut()}>Log out</Button>
-            </div>
-            {apiKey && <pre>{apiKey}</pre>}
-
-            {tokenData?.roles.includes(entity.adminRole) && (
-              <>
-                <p>Impersonate a role (UI only)</p>
-                <UnstyledList>
-                  {entity.roles.map((role: string) => (
-                    <li key={role}>
-                      <Radio
-                        label={role}
-                        name="impersonate-role"
-                        value={role}
-                        checked={checked === role}
-                        onChange={(e: any) => updateChecked(e.target.value)}
-                      />
-                    </li>
-                  ))}
-                </UnstyledList>
-              </>
-            )}
-          </div>
-        </Dialog>
+          setIsOpen={setVisibleUserInfo}
+          applicationEntity={entity}
+        />
       </TopBar.CustomContent>
     </TopBar>
   )
