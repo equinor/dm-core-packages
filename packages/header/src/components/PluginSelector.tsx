c0d20d7ab58f8d92617e9a98ca7d5d56c07c8647
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-02-06 15:27:03 +0100

    chore: start on recipeselector
    fix: use plugin Context instead of importing plugins in Header.tsx

---
+++ packages/header/src/components/PluginSelector.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import styled from 'styled-components'
+import { TUiRecipe } from '@development-framework/dm-core'
+
+const AppSelectorWrapper = styled.div`
+  position: absolute;
+  top: 60px;
+  left: 0;
+  min-width: 300px;
+  max-width: 300px;
+  background: #ffffff;
+  border: 1px solid gray;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  padding: 10px;
+  width: min-content;
+`
+
+const AppBox = styled.div`
+  border: 3px solid grey;
+  padding: 8px;
+  margin: 5px;
+  height: 80px;
+  width: 80px;
+  background: #b3dae0;
+  color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover {
+    background: #4f878d;
+  }
+`
+
+type PluginSelectorProps = {
+  selectableUiRecipeNames: string[]
+  setSelectedUiPlugin: (uiPluginName: string) => void
+  availableUiRecipes: TUiRecipe[]
+}
+export const PluginSelector = (props: PluginSelectorProps) => {
+  const {
+    selectableUiRecipeNames,
+    setSelectedUiPlugin,
+    availableUiRecipes,
+  } = props
+
+  return (
+    <AppSelectorWrapper>
+      {selectableUiRecipeNames.map((recipeName: string) => (
+        <AppBox
+          onClick={() => {
+            availableUiRecipes.map((recipe) => {
+              if (recipe.name === recipeName) {
+                setSelectedUiPlugin(recipe.plugin)
+              }
+            })
+          }}
+        >
+          {recipeName}
+        </AppBox>
+      ))}
+    </AppSelectorWrapper>
+  )
+}
