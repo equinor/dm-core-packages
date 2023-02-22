11944b2e305a3d656afc9218c7abc7f43015a59b
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-02-22 12:21:13 +0100

    refactor: move form plugin to dm-core-plugins

--- example/src/plugins.js
+++ example/src/plugins.js
@@ -1,7 +1,6 @@
 export default [
     import('@development-framework/blueprint'),
     import('@development-framework/default-pdf'),
-    import('@development-framework/form'),
     import('@development-framework/header'),
     import('@development-framework/dm-core-plugins'),
 ]
