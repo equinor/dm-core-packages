bea33177cbdf04bd0ab0fd0074c1f8e58569f37f
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-02-22 10:07:50 +0100

    refactor: move yaml plugin into dm-core-packages

--- example/src/plugins.js
+++ example/src/plugins.js
@@ -1,5 +1,4 @@
 export default [
-    import('@development-framework/yaml-view'),
     import('@development-framework/blueprint'),
     import('@development-framework/default-pdf'),
     import('@development-framework/form'),
