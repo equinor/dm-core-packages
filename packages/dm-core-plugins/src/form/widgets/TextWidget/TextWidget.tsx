98d00eefde9d86c4d0050e24f77fefb2a2e05cdd
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-02-26 13:54:11 +0100

    fix: add more props to TextField component in TextWidget

--- packages/dm-core-plugins/src/form/widgets/TextWidget/TextWidget.tsx
+++ packages/dm-core-plugins/src/form/widgets/TextWidget/TextWidget.tsx
@@ -1,24 +1,27 @@
 import React from 'react'
 
-import { TextField, Icon } from '@equinor/eds-core-react'
+import {TextField, Icon} from '@equinor/eds-core-react'
 
-import { error_filled } from '@equinor/eds-icons'
-import { TWidget } from '../../types'
+import {error_filled} from '@equinor/eds-icons'
+import {TWidget} from '../../types'
 
-Icon.add({ error_filled })
+Icon.add({error_filled})
 
 const TextWidget = (props: TWidget) => {
-  const { label, onChange } = props
+  const {label, onChange} = props
 
   const _onChange = ({
-    target: { value },
-  }: React.ChangeEvent<HTMLInputElement>) => onChange(value === '' ? '' : value)
+                       target: {value},
+                     }: React.ChangeEvent<HTMLInputElement>) => onChange(value === '' ? '' : value)
   return (
     <TextField
       id={props.id}
       readOnly={props.readOnly}
       value={props.value}
       onClick={props.onClick}
+      inputRef={props.inputRef}
+      variant={props.variant}
+      helperText={props.helperText}
       onChange={_onChange}
       label={label}
     />
