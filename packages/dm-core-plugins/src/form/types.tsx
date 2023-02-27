758dedb9cf87a031d4c5e68889d1e3b313b12b7d
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-02-27 10:21:35 +0100

    fix: type error for form plugin

--- packages/dm-core-plugins/src/form/types.tsx
+++ packages/dm-core-plugins/src/form/types.tsx
@@ -48,7 +48,7 @@ export type TBooleanFieldProps = {
   uiAttribute?: any
 }
 
-export declare type Variants = 'error' | 'success' | 'error'
+export declare type Variants = 'error' | 'success' | 'warning'
 
 export type TWidget = {
   label: string
