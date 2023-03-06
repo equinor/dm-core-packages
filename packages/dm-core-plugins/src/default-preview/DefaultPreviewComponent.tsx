a6593f43767ea33eb5053d0b286616361b9c68cc
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-03-06 14:44:55 +0100

    fix: rename default-preview plugin to json

--- packages/dm-core-plugins/src/default-preview/DefaultPreviewComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import { IUIPlugin, TPlugin } from '@development-framework/dm-core'
-import PreviewPlugin from './PreviewPlugin'
-
-export const DefaultPreviewComponent = (props: IUIPlugin) => {
-  return <PreviewPlugin idReference={props.idReference} />
-}
