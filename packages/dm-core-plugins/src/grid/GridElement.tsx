457ae72582e29911332ec2eeb2319b7070f97b3b
Eirik Ola Aksnes <eoaksnes@gmail.com>
2023-03-10 10:31:55 +0100

    feat: add grid plugin

---
+++ packages/dm-core-plugins/src/grid/GridElement.tsx
@@ -0,0 +1,34 @@
+import { TGridItem, TGridArea } from './types'
+import styled from 'styled-components'
+import React from 'react'
+import { TGenericObject, ViewCreator } from '@development-framework/dm-core'
+
+const Element = styled.div`
+  grid-area: ${(props: TGridArea) =>
+    `${props.rowStart} / ${props.columnStart} / ${props.rowEnd} / ${props.columnEnd} `};
+`
+
+type TGridItemProps = {
+  idReference: string
+  item: TGridItem
+  document: TGenericObject
+}
+
+export const GridElement = (props: TGridItemProps): JSX.Element => {
+  const { idReference, document, item } = props
+
+  return (
+    <Element
+      rowStart={item.gridArea.rowStart}
+      rowEnd={item.gridArea.rowEnd}
+      columnStart={item.gridArea.columnStart}
+      columnEnd={item.gridArea.columnEnd}
+    >
+      <ViewCreator
+        idReference={idReference}
+        document={document}
+        viewConfig={item.viewConfig}
+      />
+    </Element>
+  )
+}
---
+++ packages/dm-core-plugins/src/grid/types.ts
@@ -0,0 +1,26 @@
+import { TViewConfig, IUIPlugin } from '@development-framework/dm-core'
+
+export type TGridSize = {
+  columns: number
+  rows: number
+}
+
+export type TGridArea = {
+  rowStart: number
+  rowEnd: number
+  columnStart: number
+  columnEnd: number
+}
+
+export type TGridItem = {
+  type: string
+  gridArea: TGridArea
+  viewConfig: TViewConfig
+}
+
+export type TGridPluginConfig = IUIPlugin & {
+  config?: {
+    size: TGridSize
+    items: TGridItem[]
+  }
+}
