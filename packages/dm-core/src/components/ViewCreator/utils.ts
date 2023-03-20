4240c06e6b0e63eb1ff43516b317a189620e9891
Eirik Ola Aksnes <eoaksnes@gmail.com>
2023-03-20 10:51:15 +0100

    feat(grid): add support for nested scope path

--- packages/dm-core/src/components/ViewCreator/utils.ts
+++ packages/dm-core/src/components/ViewCreator/utils.ts
@@ -6,8 +6,19 @@ export const getTarget = (idReference: string, viewConfig: TViewConfig) => {
   return idReference
 }
 
+// Source: https://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-and-arrays-by-string-path
+const resolvePath = (
+  initial: Record<string | number, any>,
+  path: string
+): TGenericObject =>
+  path.split('.').reduce((acc, key) => {
+    return key in acc ? acc[key] : acc
+  }, initial)
+
 export const getType = (document: TGenericObject, viewConfig: TViewConfig) => {
-  // TODO: Add support for dotted paths
-  if (viewConfig?.scope) return document[viewConfig.scope]?.type
+  if (viewConfig?.scope) {
+    const target: TGenericObject = resolvePath(document, viewConfig.scope)
+    return target.type
+  }
   return document.type
 }
