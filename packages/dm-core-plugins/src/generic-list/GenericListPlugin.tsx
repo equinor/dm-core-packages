56cb5264222d45191c25bb0a63e62c5fa91ea507
Stig Ofstad <stigofstad@gmail.com>
2023-03-31 15:48:12 +0200

    WIP

--- packages/dm-core-plugins/src/generic-list/GenericListPlugin.tsx
+++ packages/dm-core-plugins/src/generic-list/GenericListPlugin.tsx
@@ -9,14 +9,40 @@ import {
   useDocument,
 } from '@development-framework/dm-core'
 import { AxiosResponse } from 'axios'
-import { Accordion, Button, Icon } from '@equinor/eds-core-react'
-import { delete_to_trash, library_add } from '@equinor/eds-icons'
+import { Button, Icon } from '@equinor/eds-core-react'
+import {
+  chevron_down,
+  chevron_right,
+  delete_to_trash,
+  library_add,
+} from '@equinor/eds-icons'
+
+import styled from 'styled-components'
+
+const TableData = styled.td`
+  text-align: right;
+`
+const TableRow = styled.tr`
+  text-align: right;
+  border-collapse: collapse;
+`
+const Table = styled.table`
+  box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%),
+    0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
+  border-radius: 4px;
+  width: 100%;
+  bordercollapse: collapse;
+`
 
 type TGenericListConfig = {
   expanded: boolean
+  headers: string[]
+  showDelete: boolean
 }
-const defaultConfig = {
+const defaultConfig: TGenericListConfig = {
   expanded: false,
+  headers: ['quantity', 'name'],
+  showDelete: true,
 }
 
 export const GenericListPlugin = (
@@ -29,37 +55,64 @@ export const GenericListPlugin = (
     2
   )
   const [items, setItems] = useState<TGenericObject[]>([])
+  const [itemsExpanded, setItemsExpanded] = useState<{
+    [key: string]: boolean
+  }>()
   const { token } = useContext(AuthContext)
   const dmssAPI = new DmssAPI(token)
 
   useEffect(() => {
     if (loading || !document) return
     // Need to generate a uuid for each item in the list to be used for reacts "key" property
-    let itemsWithIds = document.map((item: TGenericObject) => ({
-      __react_key__: crypto.randomUUID(),
-      ...item,
-    }))
+    // let itemsWithIds = document.map((item: TGenericObject) => ({
+    //   __react_key__: crypto.randomUUID(),
+    //   ...item,
+    // }))
+    // const itemsWithIds = document.reduce((array: TGenericObject[], item: TGenericObject) => ({
+    //     ...array,
+    //     [item.__react_key__]: item,
+    //   })
+    // )
+    const itemsWithIds = Object.fromEntries(
+      Object.values(document).map((value: any) => [crypto.randomUUID(), value])
+    )
+
+    const itemsExpanded = Object.fromEntries(
+      Object.keys(itemsWithIds).map((key: string) => [
+        key,
+        internalConfig.expanded,
+      ])
+    )
+    // const itemsExpanded = itemsWithIds.reduce(
+    //   (array: TGenericObject[], item: TGenericObject) => ({
+    //     ...array,
+    //     [item.__react_key__]: internalConfig.expanded,
+    //   })
+    // )
+    setItemsExpanded(itemsExpanded)
     setItems(itemsWithIds)
   }, [document, loading])
 
-  const deleteItem = (reference: string, index: number) => {
+  const deleteItem = (reference: string, key: string) => {
     dmssAPI.documentRemove({ idReference: reference }).then(() => {
-      items.splice(index, 1)
-      setItems([...items])
+      delete items[key]
+      setItems({ ...items })
     })
   }
   const addItem = (reference: string, type: string) => {
     dmssAPI
       // TODO: Get type from parent blueprint, be able to select specialised type
-      .instantiateEntity({ entity: { type: items[0].type } })
+      .instantiateEntity({
+        entity: { type: items[Object.keys(items)[0]].type },
+      })
       .then((newEntity: AxiosResponse<any>) => {
         dmssAPI
           .documentAdd({ absoluteRef: reference, body: newEntity.data })
           .then(() =>
-            setItems([
+            setItems({
               ...items,
-              { __react_key__: crypto.randomUUID(), ...newEntity.data },
-            ])
+              [crypto.randomUUID()]: { ...newEntity.data },
+            })
           )
       })
   }
@@ -78,35 +131,69 @@ export const GenericListPlugin = (
           <Icon data={library_add} title="Append" />
         </Button>
       </div>
-      <Accordion>
-        {items.map((item: any, index: number) => (
-          <Accordion.Item
-            key={item.__react_key__}
-            isExpanded={internalConfig.expanded}
-          >
-            <Accordion.Header>
-              <Accordion.HeaderTitle>
-                {`${idReference}.${index}`}
-              </Accordion.HeaderTitle>
-              <Accordion.HeaderActions>
-                <Button
-                  variant="ghost_icon"
-                  color="danger"
-                  onClick={() => deleteItem(`${idReference}.${index}`, index)}
+      <Table>
+        <thead>
+          <tr style={{ borderBottom: '1px solid rgba(224, 224, 224, 1)' }}>
+            <th></th>
+            {internalConfig.headers.map((attribute: string) => (
+              <th>{attribute}</th>
+            ))}
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(items).map(([key, item], index: number) => (
+            <React.Fragment key={key}>
+              <TableRow>
+                <td style={{ textAlign: 'left' }}>
+                  <Button
+                    variant="ghost_icon"
+                    onClick={() =>
+                      setItemsExpanded({
+                        ...itemsExpanded,
+                        [key]: !itemsExpanded[key],
+                      })
+                    }
+                  >
+                    <Icon
+                      data={itemsExpanded[key] ? chevron_down : chevron_right}
+                      title="Delete"
+                    />
+                  </Button>
+                </td>
+                {internalConfig.headers.map((attribute: string) => (
+                  <td style={{ textAlign: 'center' }}>{item[attribute]}</td>
+                ))}
+                <TableData>
+                  <Button
+                    variant="ghost_icon"
+                    color="danger"
+                    onClick={() => deleteItem(`${idReference}.${index}`, key)}
+                  >
+                    <Icon data={delete_to_trash} title="Delete" />
+                  </Button>
+                </TableData>
+              </TableRow>
+              <TableRow>
+                <td
+                  colSpan={4}
+                  style={{
+                    borderBottom: '1px solid rgba(224, 224, 224, 1)',
+                    textAlign: 'initial',
+                  }}
                 >
-                  <Icon data={delete_to_trash} title="Delete" />
-                </Button>
-              </Accordion.HeaderActions>
-            </Accordion.Header>
-            <Accordion.Panel>
-              <EntityView
-                type={item.type}
-                idReference={`${idReference}.${index}`}
-              />
-            </Accordion.Panel>
-          </Accordion.Item>
-        ))}
-      </Accordion>
+                  {itemsExpanded[key] && (
+                    <EntityView
+                      type={item.type}
+                      idReference={`${idReference}.${index}`}
+                    />
+                  )}
+                </td>
+              </TableRow>
+            </React.Fragment>
+          ))}
+        </tbody>
+      </Table>
       <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
         <Button
           variant="ghost_icon"
