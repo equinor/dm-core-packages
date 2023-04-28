2dc8a0a4e4b4636e872636a9dd8b9bc32d4a284b
Eirik Ola Aksnes <eoaksnes@gmail.com>
2023-04-28 10:06:49 +0200

    refactor: cleanup frontpage

--- example/src/App.js
+++ example/src/App.js
@@ -1,83 +1,53 @@
 import {
   EntityView,
   FSTreeContext,
-  JsonView,
-  NewEntityButton,
   TreeView,
-  useDMSS,
 } from '@development-framework/dm-core'
 import { useContext, useState } from 'react'
 
-import { Jobs } from './test_components/Jobs'
-
 function App() {
-  const dmssAPI = useDMSS()
-
   const { treeNodes, loading } = useContext(FSTreeContext)
-
-  const [createdEntity, setCreatedEntity] = useState({})
   const [selectedType, setSelectedType] = useState()
   const [selectedEntity, setSelectedEntity] = useState()
+
+  if (loading) return <>Loading</>
 
   return (
     <div
       style={{
         display: 'flex',
-        flexDirection: 'column',
-        padding: '50px',
-        justifyContent: 'space-evenly',
+        padding: '20px',
         overflow: 'auto',
       }}
     >
-      <h3>Browse examples</h3>
       <div
         style={{
-          border: '2px solid blue',
-          backgroundColor: 'whitesmoke',
-          width: '600px',
+          width: '500px',
         }}
       >
-        {loading ? (
-          <>Loading...</>
-        ) : (
-          <TreeView
-            nodes={treeNodes}
-            onSelect={(node) => {
-              setSelectedType(node.type)
-              setSelectedEntity(node.nodeId)
-            }}
+        <h3>Examples</h3>
+        <TreeView
+          nodes={treeNodes}
+          onSelect={(node) => {
+            setSelectedType(node.type)
+            setSelectedEntity(node.nodeId)
+          }}
+        />
+      </div>
+      <div
+        style={{
+          padding: '20px',
+          width: '100%',
+        }}
+      >
+        {selectedType && selectedEntity && (
+          <EntityView
+            type={selectedType}
+            idReference={selectedEntity}
+            key={selectedEntity}
           />
         )}
       </div>
-      {selectedType && selectedEntity && (
-        <EntityView
-          type={selectedType}
-          idReference={selectedEntity}
-          key={selectedEntity}
-        />
-      )}
-      <h3>Example on using jobs</h3>
-      <Jobs />
-      <h3>NewEntityButton button from dm-core</h3>
-      <NewEntityButton
-        type={'dmss://system/SIMOS/NamedEntity'}
-        defaultDestination={'DemoDataSource/DemoPackage'}
-        onCreated={(createdEntity) =>
-          dmssAPI
-            .documentGetById({
-              idReference: `DemoDataSource/${createdEntity._id}`,
-            })
-            .then((response) => {
-              setCreatedEntity(response.data)
-            })
-        }
-      />
-      {Object.entries(createdEntity).length !== 0 && (
-        <>
-          <h3>Created entity:</h3>
-          <JsonView data={createdEntity} />
-        </>
-      )}
     </div>
   )
 }
