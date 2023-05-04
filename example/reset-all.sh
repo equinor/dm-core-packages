cb718034a0455ceb1ee57175c255f370e72c527e
Stig Ofstad <stigofstad@gmail.com>
2023-05-04 11:20:47 +0200

    chore: make reset script with with compose alias

--- example/reset-all.sh
+++ example/reset-all.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -e
-docker-compose run --rm dmss reset-app
-docker-compose run --rm job-api dm -u http://dmss:5000 reset ../app
+
+# Get the available docker-compose command
+docker compose &> /dev/null
+if [[ $? == 0 ]]; then
+  compose="docker compose"
+else
+  compose="docker-compose"
+fi
+
+eval $compose run --rm dmss reset-app
+eval $compose run --rm job-api dm -u http://dmss:5000 reset ../app
 dm reset app
 dm import-plugin-blueprints node_modules/@development-framework/dm-core-plugins
 echo "Creating recipe lookup..."
