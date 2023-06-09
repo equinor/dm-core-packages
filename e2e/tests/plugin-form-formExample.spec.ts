5b4d55a6fa4c21597e2cff8fc097a2881e78d9bd
Roar Larsen <roarla@equinor.com>
2023-06-09 10:38:30 +0200

    extended e2e test and moving tsconfig

--- e2e/tests/plugin-form-formExample.spec.ts
+++ e2e/tests/plugin-form-formExample.spec.ts
@@ -1,27 +1,36 @@
 import { test, expect } from '@playwright/test'
 
-test('NestedExample', async ({ page }) => {
+test('Form Example plugin', async ({ page }) => {
   //Open form
   await page.goto('http://localhost:3000/')
   await page.getByText('plugins', { exact: true }).click()
   await page.getByText('form').click()
   await page.getByText('DemoDataSource/$formExample').click()
 
+  //A bool
+  await page.getByTestId('form-checkbox').uncheck()
+  await page.getByTestId('form-submit').click()
+  await expect(page.getByTestId('form-checkbox')).not.toBeChecked()
+
   //Add a list of string
   await page.getByTestId('add-listOfStrings').click()
-  await page
-    .locator('div')
-    .filter({ hasText: /^A list of stringsRemoveRemoveRemoveAdd$/ })
-    .getByTestId('form-textfield')
-    .nth(2)
-    .click()
-  await page
-    .locator('div')
-    .filter({ hasText: /^A list of stringsRemoveRemoveRemoveAdd$/ })
-    .getByTestId('form-textfield')
-    .nth(2)
-    .fill('test')
-  // await expect(page.getByTestId('form-textfield')).toHaveText(['test'])
-  // await page.getByRole('button', { name: 'Remove' }).nth(2).click()
-  // await expect(page.getByTestId('form-textfield')).not.toContainText('test')
+  await page.locator('input[id="listOfStrings.2"]').fill('test')
+  await page.keyboard.press('Enter')
+  await page.getByText('A list of strings')
+  await expect(page.locator('input[id="listOfStrings.2"]')).toBeVisible()
+  await expect(page.locator('input[id="listOfStrings.2"]')).toHaveValue('test')
+  await page.getByRole('button', { name: 'Remove' }).nth(2).click()
+  await expect(page.locator('input[id="listOfStrings.2"]')).not.toBeVisible()
+
+  //A nested object
+  await page.getByRole('button', { name: 'Open' }).first().click()
+  await page.getByLabel('Foo (optional)').fill('1234')
+  await page.getByLabel('Bar').fill('bar test')
+  await page.getByLabel('Baz (optional)').fill('baz test')
+  await page.getByRole('button', { name: 'Submit' }).click()
+  await page.getByText('self').first().click()
+  await page.getByRole('button', { name: 'Open' }).first().click()
+  await expect(page.getByLabel('Foo (optional)')).toHaveValue('1234')
+  await expect(page.getByLabel('Bar')).toHaveValue('bar test')
+  await expect(page.getByLabel('Baz (optional)')).toHaveValue('baz test')
 })
--- e2e/tests/signalApp.spec.ts
+++ e2e/tests/signalApp.spec.ts
@@ -1,14 +1,22 @@
 import { test, expect } from '@playwright/test'
 
 test('signalApp', async ({ page }) => {
+  //Starting app
   await page.goto('http://localhost:3000/')
   await page.getByText('apps').click()
   await page.getByText('MySignalApp').click()
   await page.getByText('signalApp', { exact: true }).click()
+
+  //Open Explorer
   await page.getByRole('banner').locator('svg').first().click()
   await page.getByText('Explorer').click()
 
   await page.getByRole('banner').locator('svg').first().click()
   await page.getByText('SideBar').click()
   await page.getByRole('button', { name: 'Open' }).click()
+
+  //Open case
+  await page.getByRole('button', { name: 'Open' }).click()
+  await page.getByRole('button', { name: 'Expand' }).click()
+  await page.getByText('editEdit').click()
 })
