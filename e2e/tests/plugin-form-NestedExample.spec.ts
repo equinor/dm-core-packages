fae1f220477ed2a013941cd37a43c01dba47ef29
Roar Larsen <roarla@equinor.com>
2023-06-09 10:38:30 +0200

    extended e2e test and moving tsconfig

--- e2e/tests/plugin-form-NestedExample.spec.ts
+++ e2e/tests/plugin-form-NestedExample.spec.ts
@@ -1,11 +1,28 @@
 import { test, expect } from '@playwright/test'
 
-test('NestedExample', async ({ page }) => {
+test('NestedExample plugin', async ({ page }) => {
+  //Open form
   await page.goto('http://localhost:3000/')
   await page.getByText('plugins', { exact: true }).click()
   await page.getByText('form').click()
   await page.getByText('DemoDataSource/$NestedExample').click()
-  await page.getByLabel('Baz (optional)').fill('something else')
+
+  //Empty current input and enter new
+  await page.getByLabel('Foo (optional)').clear()
+  await page.getByLabel('Foo (optional)').fill('1234')
+  await page.getByLabel('Bar').clear()
+  await page.getByLabel('Bar').fill('Bar')
+  await page.getByLabel('Baz (optional)').clear()
+  await page.getByLabel('Baz (optional)').fill('Something else')
   await page.getByTestId('form-submit').click()
-  // await expect(page.getByTestId('form-submit')).toHaveText(['Submit'])
+  await expect(page.getByTestId('form-submit')).toHaveText(['Submit'])
+
+  //Reloading form, expecting entered values to be stored
+  await page.reload()
+  await page.getByText('plugins', { exact: true }).click()
+  await page.getByText('form').click()
+  await page.getByText('DemoDataSource/$NestedExample').click()
+  await expect(page.getByLabel('Foo (optional)')).toHaveValue('1234')
+  await expect(page.getByLabel('Bar')).toHaveValue('Bar')
+  await expect(page.getByLabel('Baz (optional)')).toHaveValue('Something else')
 })
