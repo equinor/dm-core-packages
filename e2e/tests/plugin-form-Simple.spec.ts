b1705105a3247c09721af4865e1b519ea05aa355
Roar Larsen <roarla@equinor.com>
2023-06-26 14:14:04 +0200

    fix: expanded simple-form pluging and updated e2e

--- e2e/tests/plugin-form-Simple.spec.ts
+++ e2e/tests/plugin-form-Simple.spec.ts
@@ -8,30 +8,48 @@ test('Simple form', async ({ page }) => {
   await page.getByText('simple', { exact: true }).click()
   await page.getByText('DemoDataSource/$Simple').click()
 
-  //Fill out optional string
-  await page.getByLabel('Optional string (optional)').fill('Foo')
+  //Remove prefilled optional string
+  await page.getByLabel('Optional string (optional)').fill('')
 
   //Fill out required string
   await page.getByTestId('form-submit').click()
   await expect(page.getByText('required', { exact: true })).toBeVisible()
-  await page.getByLabel('Required string').fill('Bar')
+  await page.getByLabel('Required string').fill('Foo')
 
   //Fill out number field
   await page.getByLabel('Numbers only (optional)').fill('Text')
   await expect(page.getByText('Only digits allowed')).toBeVisible()
-  await page.getByLabel('Numbers only (optional)').fill('123')
+  await page.getByLabel('Numbers only (optional)').fill('3.14')
+
+  //Fill out integer field
+  //await page.getByLabel('Integer only (optional)').fill('3.14') //Known bug (itemid:31708452)
+  //await expect(page.getByText('Only integer allowed')).toBeVisible()
+  await page.getByLabel('Integer only (optional)').fill('123')
+
+  //Check checkbox
+  await page.getByLabel('An optional checkbox (optional)').check()
+  //await page.getByTestId('form-submit').click()
+  //await expect(page.getByText('<Field is mandatory>')).toBeVisible() //Known bug (itemid:31708452)
+  await page
+    .getByLabel('A required checbox (e.g. for confirmation purposes)')
+    .check()
 
   //Submitting form
   await page.getByTestId('form-submit').click()
   await expect(page.getByTestId('form-submit')).toHaveText(['Submit'])
 
-  // //Reloading form, expecting entered values to be stored
+  //Reloading form, expecting entered values to be stored
   await page.reload()
   await page.getByText('plugins', { exact: true }).click()
   await page.getByText('form').click()
   await page.getByText('simple', { exact: true }).click()
   await page.getByText('DemoDataSource/$Simple').click()
-  await expect(page.getByLabel('Optional string (optional)')).toHaveValue('Foo')
-  await expect(page.getByLabel('Required string')).toHaveValue('Bar')
-  await expect(page.getByLabel('Numbers only (optional)')).toHaveValue('123')
+  await expect(page.getByLabel('Optional string (optional)')).toHaveValue('')
+  await expect(page.getByLabel('Required string')).toHaveValue('Foo')
+  await expect(page.getByLabel('Numbers only (optional)')).toHaveValue('3.14')
+  await expect(page.getByLabel('Integer only (optional)')).toHaveValue('123')
+  await expect(page.getByLabel('An optional checkbox (optional)')).toBeChecked()
+  await expect(
+    page.getByLabel('A required checbox (e.g. for confirmation purposes)')
+  ).toBeChecked()
 })
