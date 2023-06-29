2d535ffb747c23abba6f8637d9d74c1a24b8b056
Andrea Vesterhus <andrea.vesterhus@bouvet.no>
2023-06-29 14:58:47 +0200

    refactor: extract table row to own component

--- packages/dm-core-plugins/src/table/types.ts
+++ packages/dm-core-plugins/src/table/types.ts
@@ -1,18 +1,20 @@
 import { TGenericObject, TViewConfig } from '@development-framework/dm-core'
 
+export type TTableFunctionalityConfig = {
+  openAsTab: boolean
+  openAsExpandable: boolean
+  add: boolean
+  sort: boolean
+  edit: boolean
+  delete: boolean
+}
+
 export type TTablePluginConfig = {
   editMode: boolean
   columns: string[]
   showDelete: boolean
   editableColumns?: string[]
-  functionality: {
-    openAsTab: boolean
-    openAsExpandable: boolean
-    add: boolean
-    sort: boolean
-    edit: boolean
-    delete: boolean
-  }
+  functionality: TTableFunctionalityConfig
   defaultView: TViewConfig
   views: TViewConfig[]
 }
@@ -34,10 +36,22 @@ export const defaultConfig: TTablePluginConfig = {
   views: [],
 }
 
-export type TTableItemRow = {
+export type TTableRowItem = {
   key: string
   data: TGenericObject
   index: number
   expanded: boolean
   isSaved: boolean
+}
+
+export type TTableRow = {
+  config: TTablePluginConfig
+  item: TTableRowItem
+  index: number
+  idReference: string
+  items: TTableRowItem[]
+  setItems: React.Dispatch<React.SetStateAction<TTableRowItem[]>>
+  setDirtyState: React.Dispatch<React.SetStateAction<boolean>>
+  onOpen: any
+  rowsPerPage: number
 }
