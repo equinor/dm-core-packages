78edb6c55e4e5e847bcd0a5bebc6b8682713a811
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-07-06 17:10:19 +0200

    refactor: improve address utils

--- packages/dm-core/src/utils/addressUtilities.ts
+++ packages/dm-core/src/utils/addressUtilities.ts
@@ -1,14 +1,49 @@
+import { splitString } from './stringUtilities'
+
 /**
- * An utility function to find dataSourceId from an address
+ * Splits an address into its respective parts
  *
- * @param address an address on the format: PROTOCOL://DATA SOURCE/<path> or DATA SOURCE/<path> or /DATA SOURCE/<path>
+ * @param address An absolute address. Valid formats include (both DOCUMENT_PATH and ATTRIBUTE_PATH is optional):
+ *  PROTOCOL://DATA_SOURCE/DOCUMENT_PATH.ATTRIBUTE_PATH
+ *  /DATA_SOURCE/DOCUMENT_PATH.ATTRIBUTE_PATH
+ *  DATA_SOURCE/DOCUMENT_PATH.ATTRIBUTE_PATH
  */
-export function getDataSourceIdFromAddress(address: string): string {
-  if (address.includes('://')) {
-    return address.split('/', 3)[2]
-  } else if (address[0] === '/') {
-    return address.split('/', 2)[1]
-  } else {
-    return address.split('/', 2)[0]
-  }
+export const splitAddress = (address: string) => {
+  const protocol = address.includes('://') ? address.split('://', 1)[0] : ''
+  const addressWithoutProtocol = address.includes('://')
+    ? address.split('://', 2)[1]
+    : address.replace(/^[/. ]+|[/. ]+$/g, '')
+  const [dataSource, path] = addressWithoutProtocol.includes('/')
+    ? splitString(addressWithoutProtocol, '/', 1)
+    : [addressWithoutProtocol, '']
+  const [documentPath, attributePath] = path.includes('.')
+    ? splitString(path, '.', 1)
+    : [path, '']
+  return { protocol, dataSource, documentPath, attributePath }
+}
+
+/**
+ *
+ * @param address A relative address. Valid formats include (ATTRIBUTE_PATH is optional):
+ * PROTOCOL://DATA_SOURCE/DOCUMENT_PATH.ATTRIBUTE_PATH
+ * /DOCUMENT_PATH.ATTRIBUTE_PATH
+ * DOCUMENT_PATH.ATTRIBUTE_PATH
+ * ^.ATTRIBUTE_PATH
+ * @param fallbackDocumentPath
+ * @param dataSource
+ * @returns
+ */
+export const resolveRelativeAddress = (
+  address: string,
+  fallbackDocumentPath: string,
+  dataSource: string
+) => {
+  address = address.replace(/^[/. ]+|[/. ]+$/g, '')
+  if (address.includes('://')) return address
+  const [documentPath, attributePath] = address.includes('.')
+    ? splitString(address, '.', 1)
+    : [address, '']
+  return `/${dataSource}/${
+    documentPath == '^' ? fallbackDocumentPath : documentPath
+  }${attributePath ? '.' + attributePath : ''}`
 }
