515359251f9c2d0773b7e4be8cec69ecb778e2bb
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-07-06 13:06:24 +0200

    fix: support /DS/<path> syntax in getDataSourceIdFromReference()

--- packages/dm-core/src/utils/referenceUtilities.ts
+++ packages/dm-core/src/utils/referenceUtilities.ts
@@ -1,11 +1,13 @@
 /**
  * An utility function to find dataSourceId from a reference
  *
- * @param reference a reference on the format: PROTOCOL://DATA SOURCE/<path>  DATA SOURCE/<path>
+ * @param reference a reference on the format: PROTOCOL://DATA SOURCE/<path> or DATA SOURCE/<path> or /DATA SOURCE/<path>
  */
 export function getDataSourceIdFromReference(reference: string): string {
   if (reference.includes('://')) {
     return reference.split('/', 3)[2]
+  } else if (reference[0] === '/') {
+    return reference.split('/', 2)[1]
   } else {
     return reference.split('/', 2)[0]
   }
