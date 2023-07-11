2922625ef73706ccf8a5013372d096dd87426260
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-07-11 09:02:07 +0200

    fix: build calculate signal container

--- example/job_handlers/signal-app-azure-container-job/publish-image.sh
+++ example/job_handlers/signal-app-azure-container-job/publish-image.sh
@@ -2,5 +2,5 @@
 # Note: you need access to the docker repo datamodelingtool.azurecr.io to be able to push the image.
 # (write "docker login datamodelingtool.azurecr.io" and supply username and password)
 
-docker build -t datamodelingtool.azurecr.io/dmt-job/generate-signal .
+docker build --platform=linux/amd64 -t datamodelingtool.azurecr.io/dmt-job/generate-signal .
 docker push datamodelingtool.azurecr.io/dmt-job/generate-signal
