07ac66fbfd114798669b075b0543763fb1782d40
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-08-03 10:46:35 +0200

    refactor: clean up File plugin (add TFileEntity type and  fix bug with extracting data source name)

--- packages/dm-core-plugins/src/file/FilePlugin.tsx
+++ packages/dm-core-plugins/src/file/FilePlugin.tsx
@@ -3,46 +3,45 @@ import * as React from 'react'
 import {
   IUIPlugin,
   Loading,
-  TGenericObject,
-  TLinkReference,
   useDocument,
   EBlueprint,
+  TFileEntity,
+  splitAddress,
+  TLinkReference,
 } from '@development-framework/dm-core'
-import { DownloadButton } from './DownloadFileButton'
+import { DownloadFileButton } from './DownloadFileButton'
 import { UploadFileButton } from './UploadFileButton'
 
 export const FilePlugin = (props: IUIPlugin) => {
   const { idReference } = props
-  const [fileEntity, loading, updateDocument, error] =
-    useDocument<TGenericObject>(idReference, 999)
-  const dataSource = idReference.split('/')[0]
-
+  const [fileEntity, loading, updateDocument, error] = useDocument<TFileEntity>(
+    idReference,
+    999
+  )
+  const { dataSource } = splitAddress(idReference)
   if (error) throw new Error(JSON.stringify(error, null, 2))
   if (loading || fileEntity === null) return <Loading />
-  if (fileEntity['type'] !== EBlueprint.FILE) return <>Error: Not File type</>
+  if (fileEntity.type !== EBlueprint.FILE) return <>Error: Not File type</>
 
   const handleUpload = (file: File, reference: TLinkReference) => {
     // Need to remove the file extension since name has strict validation rules.
-    fileEntity['name'] = file.name
-      .split('/')
-      .slice(-1)
-      .join()
-      .split('.')
-      .shift()
-    fileEntity['size'] = file.size
-    fileEntity['filetype'] = file.type
-    fileEntity['date'] = new Date()
-    fileEntity['content'] = reference
+    //  TODO renaming of an exisitng entity does not work
+    //fileEntity.name =
+    //  file.name.split('/').slice(-1).join().split('.').shift() ?? ''
+    fileEntity.size = file.size
+    fileEntity.filetype = file.type
+    fileEntity.date = new Date().toDateString()
+    fileEntity.content = reference
     updateDocument(fileEntity, false)
   }
 
   return (
     <>
       {fileEntity?.content && (
-        <DownloadButton fileEntity={fileEntity} dataSourceId={dataSource} />
+        <DownloadFileButton fileEntity={fileEntity} dataSourceId={dataSource} />
       )}
       {fileEntity?.content == undefined && (
-        <UploadFileButton onUpload={handleUpload} />
+        <UploadFileButton onUpload={handleUpload} dataSourceId={dataSource} />
       )}
     </>
   )
