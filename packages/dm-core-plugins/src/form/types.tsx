87b31b58ae775133845684122fb58021d5ed8641
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-08-04 13:32:00 +0200

    feat(form): allow inline object list
    ref #276

--- packages/dm-core-plugins/src/form/types.tsx
+++ packages/dm-core-plugins/src/form/types.tsx
@@ -33,6 +33,14 @@ export type TContentProps = {
   uiAttribute: TAttributeConfig | undefined
 }
 
+export type TArrayFieldProps = {
+  namePath: string
+  displayLabel: string
+  type: string
+  uiAttribute: TAttributeConfig | undefined
+  dimensions: string | undefined
+}
+
 export type TAttributeFieldProps = {
   namePath: string
   attribute: TAttribute
