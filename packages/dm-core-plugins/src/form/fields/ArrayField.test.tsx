5f06065551504f9f1e870f25d47735670f9d7216
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-08-08 10:46:44 +0200

    test(form): make tests pass
    ref #276

--- packages/dm-core-plugins/src/form/fields/ArrayField.test.tsx
+++ packages/dm-core-plugins/src/form/fields/ArrayField.test.tsx
@@ -1,49 +1,30 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { TViewConfig } from '@development-framework/dm-core'
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react'
 import React from 'react'
 import { Form } from '../components/Form'
 import { mockBlueprintGet, wrapper } from '../test-utils'
+import { TConfig } from '../types'
 
-describe('ArrayField', () => {
-  afterEach(() => {
-    jest.clearAllMocks()
-  })
+const EntityViewMock = jest.fn()
+jest.mock('@development-framework/dm-core', () => ({
+  __esModule: true,
+  ...jest.requireActual('@development-framework/dm-core'),
+  EntityView: (props: any) => {
+    EntityViewMock(props)
+    return <div data-testid="mock-EntityView" />
+  },
+}))
 
-  describe('Unsupported array', () => {
-    it('should warn on wrong attribute type', async () => {
-      mockBlueprintGet([
-        {
-          name: 'MyBlueprint',
-          type: 'system/SIMOS/Blueprint',
-          attributes: [
-            {
-              name: 'foo',
-              type: 'system/SIMOS/BlueprintAttribute',
-              attributeType: 'invalid',
-              dimensions: '*',
-            },
-          ],
-        },
-      ])
-      const formData = {
-        foo: [
-          {
-            type: 'invalid',
-          },
-        ],
-      }
-      render(
-        <Form idReference="ds/$1" type="MyBlueprint" formData={formData} />,
-        {
-          wrapper,
-        }
-      )
-      await waitFor(() => {
-        expect(screen.getByText('Could not find the blueprint')).toBeDefined()
-      })
-    })
-  })
+afterEach(() => cleanup())
 
-  describe('List of strings', () => {
+describe('List of strings', () => {
+  const setup = async (data?: any) => {
     const blueprint = {
       name: 'MyBlueprint',
       type: 'system/SIMOS/Blueprint',
@@ -56,118 +37,76 @@ describe('ArrayField', () => {
         },
       ],
     }
-
-    it('should contain no field in the list by default', async () => {
-      mockBlueprintGet([blueprint])
-      const { container } = render(
-        <Form idReference="ds/$1" type="MyBlueprint" />,
-        { wrapper }
-      )
-      await waitFor(() => {
-        expect(container.querySelectorAll(` input[type=text]`).length).toBe(0)
-      })
+    mockBlueprintGet([blueprint])
+    const utils = render(
+      <Form idReference="ds/$1" type="MyBlueprint" formData={data} />,
+      { wrapper }
+    )
+    return await waitFor(() => {
+      const addButton = screen.getByRole('button', { name: 'Add' })
+      return { ...utils, addButton }
     })
+  }
 
-    it('should have an add button', async () => {
-      mockBlueprintGet([blueprint])
-      render(<Form idReference="ds/$1" type="MyBlueprint" />, { wrapper })
-      await waitFor(() => {
-        expect(screen.getByText('Add')).toBeDefined()
-      })
-    })
-
-    it('should add a new field when clicking the add button', async () => {
-      mockBlueprintGet([blueprint])
-      const { container } = render(
-        <Form idReference="ds/$1" type="MyBlueprint" />,
-        { wrapper }
-      )
-      await waitFor(() => {
-        fireEvent.click(screen.getByText('Add'))
-        expect(container.querySelectorAll(` input[type=text]`).length).toBe(1)
-      })
-    })
-
-    it('should fill an array field with data', async () => {
-      mockBlueprintGet([blueprint])
-      const formData = {
-        array: ['foo', 'bar'],
-      }
-      const { container } = render(
-        <Form idReference="ds/$1" type="MyBlueprint" formData={formData} />,
-        { wrapper }
-      )
-      await waitFor(() => {
-        const inputs = container.querySelectorAll(` input[type=text]`)
-        expect(inputs.length).toBe(2)
-        expect(inputs[0].getAttribute('value')).toBe('foo')
-        expect(inputs[1].getAttribute('value')).toBe('bar')
-      })
-    })
-
-    it('should render the input widgets with the expected ids', async () => {
-      mockBlueprintGet([blueprint])
-      const formData = {
-        array: ['foo', 'bar'],
-      }
-      const { container } = render(
-        <Form idReference="ds/$1" type="MyBlueprint" formData={formData} />,
-        { wrapper }
-      )
-      await waitFor(() => {
-        const inputs = container.querySelectorAll(` input[type=text]`)
-        expect(inputs.length).toBe(2)
-        expect(inputs[0].id).toBe('array.0')
-        expect(inputs[1].id).toBe('array.1')
-      })
-    })
-
-    it('should render nested type with the expected ids', async () => {
-      const parentBlueprint = {
-        name: 'MyBlueprint',
-        type: 'system/SIMOS/Blueprint',
-        attributes: [
-          {
-            name: 'array',
-            type: 'system/SIMOS/BlueprintAttribute',
-            attributeType: 'Item',
-            dimensions: '*',
-          },
-        ],
-      }
-      const nestedBlueprint = {
-        name: 'Item',
-        type: 'system/SIMOS/Blueprint',
-        attributes: [
-          {
-            name: 'foo',
-            type: 'system/SIMOS/BlueprintAttribute',
-            attributeType: 'string',
-          },
-        ],
-      }
-
-      mockBlueprintGet([parentBlueprint, nestedBlueprint])
-      const formData = {
-        array: [
-          {
-            foo: 'foo',
-          },
-        ],
-      }
-      const { container } = render(
-        <Form idReference="ds/$1" type="MyBlueprint" formData={formData} />,
-        { wrapper }
-      )
-      await waitFor(() => {
-        const inputs = container.querySelectorAll(` input[type=text]`)
-        expect(inputs.length).toBe(1)
-        expect(inputs[0].id).toBe('array.0.foo')
-      })
+  it('should contain no field in the list by default', async () => {
+    await setup()
+    await waitFor(() => {
+      const inputs = screen.queryAllByTestId('form-textfield')
+      expect(inputs.length).toBe(0)
     })
   })
 
-  describe('Nested lists', () => {
+  it('should have an add button', async () => {
+    const utils = await setup()
+    await waitFor(() => {
+      expect(utils.addButton).toBeDefined()
+    })
+  })
+
+  it('should add a new field when clicking the add button', async () => {
+    const utils = await setup()
+    await waitFor(() => {
+      const inputs = screen.queryAllByTestId('form-textfield')
+      fireEvent.click(utils.addButton)
+      expect(inputs.length).toBe(1)
+    })
+  })
+
+  it('should fill an array field with data', async () => {
+    const formData = {
+      array: ['foo', 'bar'],
+    }
+    await setup(formData)
+    await waitFor(() => {
+      const inputs = screen.queryAllByTestId('form-textfield')
+      expect(inputs.length).toBe(2)
+      expect(inputs[0].getAttribute('value')).toBe('foo')
+      expect(inputs[1].getAttribute('value')).toBe('bar')
+    })
+  })
+
+  it('should render the input widgets with the expected ids', async () => {
+    const formData = {
+      array: ['foo', 'bar'],
+    }
+    await setup(formData)
+    await waitFor(() => {
+      const inputs = screen.queryAllByTestId('form-textfield')
+      expect(inputs.length).toBe(2)
+      expect(inputs[0].id).toBe('array.0')
+      expect(inputs[1].id).toBe('array.1')
+    })
+  })
+})
+
+describe('List of objects', () => {
+  const setup = async ({
+    config,
+    onOpen,
+  }: {
+    config?: TConfig
+    onOpen?: (key: string, view: TViewConfig) => void
+  }) => {
     const outer = {
       name: 'Root',
       type: 'system/SIMOS/Blueprint',
@@ -192,40 +131,93 @@ describe('ArrayField', () => {
         },
       ],
     }
+    mockBlueprintGet([outer, inner])
+    const formData = {
+      array: [
+        {
+          foo: 'foo',
+        },
+      ],
+    }
+    const utils = render(
+      <Form
+        idReference="ds/$1"
+        type="Root"
+        formData={formData}
+        config={config}
+        onOpen={onOpen}
+      />,
+      {
+        wrapper,
+      }
+    )
+    const openButton = screen.findByRole('button', { name: 'Open' })
+    const entityView = screen.findByTestId('mock-EntityView')
+    return { ...utils, openButton, entityView }
+  }
 
-    it('should render two lists of inputs inside of a list', async () => {
-      mockBlueprintGet([outer, inner])
-      const formData = {
-        array: [
+  it('should call EntityView if there is no onOpen function', async () => {
+    const utils = await setup({
+      config: {
+        attributes: [
           {
-            foo: ['1', '2', '3'],
+            type: 'PLUGINS:dm-core-plugins/form/fields/ArrayField',
+            name: 'array',
+            uiRecipe: 'aRecipe',
           },
         ],
-      }
-      const { container } = render(
-        <Form idReference="ds/$1" type="Root" formData={formData} />,
-        {
-          wrapper,
-        }
-      )
-      await waitFor(() => {
-        const inputs = container.querySelectorAll(` input[type=text]`)
-        expect(inputs.length).toBe(3)
-        expect(inputs[0].id).toBe('array.0.foo.0')
-        expect(inputs[1].id).toBe('array.0.foo.1')
-        expect(inputs[2].id).toBe('array.0.foo.2')
-      })
+        fields: [],
+      },
     })
+    await waitFor(() => {
+      expect(utils.entityView).toBeDefined()
+      expect(EntityViewMock).toHaveBeenLastCalledWith(
+        expect.objectContaining({
+          dimensions: '*',
+          idReference: 'ds/$1.array',
+          onOpen: undefined,
+          recipeName: 'aRecipe',
+          type: 'Item',
+        })
+      )
+    })
+  })
 
-    it.skip('should add an inner when clicking the add button', async () => {
-      mockBlueprintGet([outer, inner])
-      const { container } = render(<Form idReference="ds/$1" type="Root" />, {
-        wrapper,
-      })
-      await waitFor(() => {
-        fireEvent.click(screen.getByTestId('add-array'))
-        expect(container.querySelectorAll(` input[type=text]`).length).toBe(1)
-      })
+  it('should show Open button if there is a onOpen button', async () => {
+    const onOpen = jest.fn()
+    const utils = await setup({ onOpen: onOpen })
+    await waitFor(() => {
+      expect(utils.openButton).toBeDefined()
+    })
+  })
+
+  it('should show EntityView if config states it', async () => {
+    const onOpen = jest.fn()
+    const utils = await setup({
+      onOpen: onOpen,
+      config: {
+        attributes: [
+          {
+            type: 'PLUGINS:dm-core-plugins/form/fields/ArrayField',
+            name: 'array',
+            uiRecipe: 'aRecipe',
+            showInline: true,
+          },
+        ],
+        fields: [],
+      },
+    })
+    await waitFor(() => {
+      expect(utils.entityView).toBeDefined()
+      expect(EntityViewMock).toHaveBeenLastCalledWith(
+        expect.objectContaining({
+          dimensions: '*',
+          idReference: 'ds/$1.array',
+          onOpen: onOpen,
+          recipeName: 'aRecipe',
+          type: 'Item',
+        })
+      )
     })
   })
 })
