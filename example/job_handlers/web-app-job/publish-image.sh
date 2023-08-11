c122e757300e881f2472b1ac33384881ce3323a5
KristianKjerstad <kristian.kjerstad@bouvet.no>
2023-08-11 14:50:47 +0200

    wip: web app jobs

---
+++ example/job_handlers/web-app-job/publish-image.sh
@@ -0,0 +1,6 @@
+#build and publish the calculate signal example Docker image
+# Note: you need access to the docker repo datamodelingtool.azurecr.io to be able to push the image.
+# (write "docker login datamodelingtool.azurecr.io" and supply username and password)
+
+docker build --platform=linux/amd64 -t datamodelingtool.azurecr.io/dmt-job/generate-signal .
+docker push datamodelingtool.azurecr.io/dmt-job/generate-signal
