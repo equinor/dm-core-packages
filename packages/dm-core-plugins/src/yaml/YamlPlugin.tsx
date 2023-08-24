4ecb5fe2242240e7f110933f6cf1faadc193d2d5
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-08-24 09:00:06 +0200

    refactor: remove button, header and clipboard copy

--- packages/dm-core-plugins/src/yaml/YamlPlugin.tsx
+++ packages/dm-core-plugins/src/yaml/YamlPlugin.tsx
@@ -1,47 +1,21 @@
-import React, { useState } from 'react'
-import hljs from 'highlight.js'
-import yaml from 'highlight.js/lib/languages/yaml'
-// @ts-ignore
-import { CopyToClipboard } from 'react-copy-to-clipboard'
-import jsyaml from 'js-yaml'
-import styled, { keyframes } from 'styled-components'
 import {
-  Button,
   IUIPlugin,
   Loading,
   TGenericObject,
   useDocument,
 } from '@development-framework/dm-core'
+import { Button } from '@equinor/eds-core-react'
+import hljs from 'highlight.js'
+import yaml from 'highlight.js/lib/languages/yaml'
+import jsyaml from 'js-yaml'
+import React from 'react'
+import { toast } from 'react-toastify'
 import './index.css'
 
 hljs.registerLanguage('yaml', yaml)
 
-const tempVisible = keyframes`
-  0%, 100% {
-    opacity: 0;
-  }
-  10%, 90% {
-    opacity: 1;
-  }
-`
-const TooltipText = styled.div`
-  font-size: 0.7rem;
-  width: 60px;
-  height: 20px;
-  background-color: #c1cae0;
-  color: #000000;
-  text-align: center;
-  padding: 2px 0;
-  margin-bottom: 6px;
-  border-radius: 3px;
-  border: black solid 1px;
-  z-index: 1;
-  animation: ${tempVisible} 2s;
-`
-
 const YamlView = (props: { document: TGenericObject }) => {
   const { document } = props
-  const [showTooltip, setShowTooltip] = useState<boolean>(false)
   const asYAML: string = jsyaml.dump(document)
   const highlighted = hljs.highlight(asYAML, { language: 'yaml' })
 
@@ -54,20 +28,25 @@ const YamlView = (props: { document: TGenericObject }) => {
           flexDirection: 'column',
         }}
       >
-        <div style={{ position: 'absolute', right: '120px' }}>
-          {showTooltip && (
-            <TooltipText onAnimationEnd={() => setShowTooltip(false)}>
-              Copied!
-            </TooltipText>
-          )}
-        </div>
-        <div>
-          <CopyToClipboard text={asYAML}>
-            <Button onClick={() => setShowTooltip(true)}>Copy as YAML</Button>
-          </CopyToClipboard>
-          <CopyToClipboard text={JSON.stringify(document)}>
-            <Button onClick={() => setShowTooltip(true)}>Copy as JSON</Button>
-          </CopyToClipboard>
+        <div style={{ display: 'flex', gap: '1rem', margin: '0.5rem 0' }}>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              navigator.clipboard.writeText(asYAML)
+              toast.success('Copied!')
+            }}
+          >
+            Copy as YAML
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              navigator.clipboard.writeText(JSON.stringify(document))
+              toast.success('Copied!')
+            }}
+          >
+            Copy as JSON
+          </Button>
         </div>
       </div>
       <pre style={{ backgroundColor: '#193549', color: 'coral', margin: '0' }}>
