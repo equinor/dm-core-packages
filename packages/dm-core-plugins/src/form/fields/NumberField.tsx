4949e6cd9ced13e7590b877cebd2e3aa09b127da
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-08-31 10:20:41 +0200

    fix(form): add validation for integers
    ref #466

--- packages/dm-core-plugins/src/form/fields/NumberField.tsx
+++ packages/dm-core-plugins/src/form/fields/NumberField.tsx
@@ -3,35 +3,27 @@ import { Controller } from 'react-hook-form'
 import { getWidget } from '../context/WidgetContext'
 import { TNumberFieldProps } from '../types'
 
-// Taken from: https://github.com/rjsf-team/react-jsonschema-form/blob/cff979dae5348e9b100447641bcb53374168367f/packages/core/src/utils.js#L436
-export const asNumber = (value: string): number | string => {
-  if (value === '') {
-    return value
-  }
-  if (/\.$/.test(value)) {
-    // "3." can't really be considered a number even if it parses in js. The
-    // user is most likely entering a float.
-    return value
-  }
-  if (/\.0$/.test(value)) {
-    // we need to return this as a string here, to allow for input like 3.07
-    return value
-  }
-  const n = Number(value)
-  const valid = typeof n === 'number' && !Number.isNaN(n)
+const REGEX_INTEGER = /^\d+([eE][-+]?\d+)?$/
+const REGEX_FLOAT = /^\d+(\.\d+)?([eE][-+]?\d+)?$/
 
-  if (/\.\d*0$/.test(value)) {
-    // It's a number, that's cool - but we need it as a string so it doesn't screw
-    // with the user when entering dollar amounts or other values (such as those with
-    // specific precision or number of significant digits)
-    return value
-  }
-
-  return valid ? n : value
+const parse = (value: unknown, isInteger: boolean): number | string | null => {
+  if (value == null) return null
+  const valueAsString = String(value)
+  const valid = isInteger
+    ? REGEX_INTEGER.test(valueAsString)
+    : REGEX_FLOAT.test(valueAsString)
+  return valid ? Number(valueAsString) : valueAsString
 }
 
 export const NumberField = (props: TNumberFieldProps) => {
-  const { namePath, displayLabel, defaultValue, optional, uiAttribute } = props
+  const {
+    namePath,
+    displayLabel,
+    defaultValue,
+    optional,
+    uiAttribute,
+    isInteger,
+  } = props
 
   const Widget = getWidget(uiAttribute?.widget ?? 'TextWidget')
 
@@ -40,27 +32,21 @@ export const NumberField = (props: TNumberFieldProps) => {
       name={namePath}
       rules={{
         required: !optional,
-        pattern: { value: /^[0-9]+$/g, message: 'Only digits allowed' },
+        pattern: {
+          value: isInteger ? REGEX_INTEGER : REGEX_FLOAT,
+          message: isInteger ? 'Only integers allowed' : 'Only numbers allowed',
+        },
       }}
       defaultValue={defaultValue || null}
       render={({
         field: { ref, onChange, ...props },
         fieldState: { invalid, error },
       }) => {
-        // Convert to number
-        const handleChange = (value: string | null) => {
-          if (value === null) {
-            onChange(null)
-          } else {
-            onChange(asNumber(value))
-          }
-        }
         return (
           <Widget
             {...props}
-            onChange={handleChange}
+            onChange={(v) => onChange(parse(v, isInteger))}
             value={props.value ?? ''}
-            type="number"
             id={namePath}
             label={displayLabel}
             inputRef={ref}
