8b932cfe38669f83a8c83f35b44c95f305004880
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-09-06 15:54:19 +0200

    fix: beautify eds dialog

--- packages/dm-core/src/components/Dialog.tsx
+++ packages/dm-core/src/components/Dialog.tsx
@@ -1,18 +1,5 @@
-import { Dialog as EdsDialog, Icon } from '@equinor/eds-core-react'
+import { Button, Dialog as EdsDialog } from '@equinor/eds-core-react'
 import React from 'react'
-import styled from 'styled-components'
-import { close } from '@equinor/eds-icons'
-
-Icon.add({
-  close,
-})
-
-const ClickableIcon = styled.div`
-  &:hover {
-    color: gray;
-    cursor: pointer;
-  }
-`
 
 export interface Props {
   /** Provides a url for avatars being used as a link. */
@@ -20,13 +7,15 @@ export interface Props {
   /** Whether the Dialog is open or not */
   isOpen: boolean
   /** Child components to display inside the Dialog */
-  children: any
+  children?: any
   /** The title of the Dialog window */
   header: string
   /** The width of the Dialog window */
   width?: string
   /** The height of the Dialog window */
   height?: string
+  /** Any buttons you'd like shown in the dialog footer */
+  actions?: JSX.Element[]
 }
 
 /**
@@ -58,7 +47,7 @@ export interface Props {
  * @returns A React component
  */
 export const Dialog = (props: Props) => {
-  const { closeScrim, isOpen, children, header, width, height } = props
+  const { closeScrim, isOpen, children, header, width, height, actions } = props
   return (
     <EdsDialog
       // @ts-ignore
@@ -69,35 +58,25 @@ export const Dialog = (props: Props) => {
         width: width ? width : '100%',
         height: height ? height : '100%',
         overflow: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
       }}
     >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            borderBottom: '#E6E6E6 1px solid',
-            paddingTop: '10px',
-          }}
-        >
-          <h3 style={{ paddingLeft: '20px' }}>{header}</h3>
-          <ClickableIcon
-            style={{
-              float: 'right',
-              paddingRight: '20px',
-            }}
-            onClick={closeScrim}
-          >
-            <Icon name="close" size={24} title="Close" />
-          </ClickableIcon>
-        </div>
+      <EdsDialog.Header>
+        <EdsDialog.Title>{header}</EdsDialog.Title>
+      </EdsDialog.Header>
+      <EdsDialog.CustomContent style={{ flexGrow: 2 }}>
         {children}
-      </div>
+      </EdsDialog.CustomContent>
+      <EdsDialog.Actions
+        style={{ display: 'flex', gap: '1rem', alignSelf: 'auto' }}
+      >
+        {actions}
+        <Button variant="ghost" onClick={closeScrim}>
+          Cancel
+        </Button>
+      </EdsDialog.Actions>
     </EdsDialog>
   )
 }
