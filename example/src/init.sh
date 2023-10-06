4692ddc1a5dc8eaae9baf1881ce42c7e810a44d6
Eirik Ola Aksnes <eoaksnes@gmail.com>
2023-10-06 12:59:41 +0200

    fix: skip from config flag

--- example/src/init.sh
+++ example/src/init.sh
@@ -4,7 +4,7 @@ set -eu
 
 if [ "$1" = 'start' ]; then
   echo "Running in Radix environment: $RADIX_ENVIRONMENT"
-  yarn build --mode ${RADIX_ENVIRONMENT:-'local'}
+  ALIAS=off yarn build --mode ${RADIX_ENVIRONMENT:-'local'}
   npm run serve
 else
   exec "$@"
