7011dd09b38d3216120d8815c414d9d382525d77
Eirik Ola Aksnes <eoaksnes@gmail.com>
2023-10-23 18:44:28 +0200

    feat(form): switch widget

---
+++ packages/dm-core-plugins/src/form/widgets/SwitchWidget.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Switch } from '@equinor/eds-core-react'
+import { TWidget } from '../types'
+
+const SwitchWidget = (props: TWidget) => {
+  const { value, readOnly } = props
+
+  return (
+    <Switch
+      {...props}
+      disabled={readOnly}
+      defaultChecked={typeof value === 'undefined' ? false : value}
+      data-testid="form-switch"
+    />
+  )
+}
+
+export default SwitchWidget
