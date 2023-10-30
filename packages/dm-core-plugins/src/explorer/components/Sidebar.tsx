f434fa398f96e97d89152a4ea81d3a3e899099e0
Henrik Bossart <henrikbossart@users.noreply.github.com>
2023-10-30 09:12:21 +0100

    feat: resizable explorer sidebar

---
+++ packages/dm-core-plugins/src/explorer/components/Sidebar.tsx
@@ -0,0 +1,51 @@
+import React, { ReactNode, useEffect, useRef, useState } from 'react'
+
+const [minWidth, maxWidth, defaultWidth] = [200, 500, 250]
+
+type TProps = {
+  children: ReactNode
+}
+
+export default function Sidebar(props: TProps) {
+  const { children } = props
+  const [width, setWidth] = useState<number>(defaultWidth)
+  const isResized = useRef<boolean>(false)
+
+  useEffect(() => {
+    window.addEventListener('mousemove', (e) => {
+      if (!isResized.current) {
+        return
+      }
+
+      setWidth((previousWidth: number) => {
+        const newWidth = previousWidth + e.movementX / 2
+        const isWidthInRange = newWidth >= minWidth && newWidth <= maxWidth
+        return isWidthInRange ? newWidth : previousWidth
+      })
+    })
+
+    window.addEventListener('mouseup', () => {
+      isResized.current = false
+    })
+  }, [])
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <div style={{ width: `${width / 16}rem`, overflow: 'hidden' }}>
+        {children}
+      </div>
+
+      {/* Handle */}
+      <div
+        style={{
+          width: '.1rem',
+          backgroundColor: 'gray',
+          cursor: 'col-resize',
+        }}
+        onMouseDown={() => {
+          isResized.current = true
+        }}
+      />
+    </div>
+  )
+}
