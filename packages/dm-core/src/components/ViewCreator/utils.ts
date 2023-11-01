6050d82a383fc7c45231a93134de138b39174bb7
Christopher Collin Løkken <chcl@equinor.com>
2023-11-01 10:19:10 +0100

    fix: disallow hat operator in scope

--- packages/dm-core/src/components/ViewCreator/utils.ts
+++ packages/dm-core/src/components/ViewCreator/utils.ts
@@ -5,7 +5,9 @@ export const getTarget = (idReference: string, viewConfig: TViewConfig) => {
     if (viewConfig.scope.slice(0, 5) == 'self.') {
       return `${idReference}.${viewConfig.scope.slice(5, -1)}`
     } else if (viewConfig.scope.slice(0, 1) == '^') {
-      return `${idReference}.${viewConfig.scope.slice(1, -1)}`
+      throw new Error(
+        `'^' operator is not allowed in 'scope' (location: ${idReference})`
+      )
     } else if (viewConfig.scope.slice(0, 1) == '.') {
       return `${idReference}${viewConfig.scope}`
     } else {
