3f91c43c72cef91a199276e911a41ecf8635d41d
Roar Larsen <roarla@equinor.com>
2023-11-10 13:26:23 +0100

    test(roles): integration test for roles

---
+++ e2e/tests/plugin-view_selector-roles_school_example.spec.ts
@@ -0,0 +1,49 @@
+import { expect, test } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+  await page.getByRole('button', { name: 'DemoDataSource' }).click()
+  await page.getByRole('button', { name: 'plugins' }).click()
+  await page.getByRole('button', { name: 'view_selector' }).click()
+  await page.getByRole('button', { name: 'roles_school_example' }).click()
+  await page.getByRole('button', { name: 'HogwartsMeta' }).click()
+})
+
+test('Admin role', async ({ page }) => {
+  await expect(
+    page.getByRole('tab', { name: 'Hogwarts Admin', exact: true })
+  ).toBeVisible()
+  await expect(
+    page.getByRole('tab', { name: 'Hogwarts All', exact: true })
+  ).toBeVisible()
+  await expect(
+    page.getByTestId('name').getByTestId('form-textfield')
+  ).toBeEditable()
+  await page.getByRole('tab', { name: 'Hogwarts All', exact: true }).click()
+  await expect(page.getByRole('tabpanel').locator('#name')).not.toBeEditable()
+})
+
+test('Change role to operator and back', async ({ page }) => {
+  await expect(
+    page.getByRole('tab', { name: 'Hogwarts Admin', exact: true })
+  ).toBeVisible()
+  await page.getByRole('button', { name: 'User' }).click()
+  await page.getByLabel('operator').check()
+  await page.getByRole('button', { name: 'Cancel' }).click() //We need a save or close button here. "Cancel" is misleading.
+  await expect(
+    page.getByRole('tab', { name: 'Hogwarts All', exact: true })
+  ).toBeVisible()
+  await expect(
+    page.getByRole('tab', { name: 'Hogwarts Admin', exact: true })
+  ).not.toBeVisible()
+  await expect(
+    page.getByTestId('name').getByTestId('form-textfield')
+  ).not.toBeEditable()
+  await page.getByRole('button', { name: 'User' }).click()
+  await page.getByLabel('admin').check()
+  await page.getByRole('button', { name: 'Cancel' }).click()
+  await page.getByRole('tab', { name: 'Hogwarts Admin', exact: true }).click()
+  await expect(
+    page.getByTestId('name').getByTestId('form-textfield')
+  ).toBeEditable()
+})
