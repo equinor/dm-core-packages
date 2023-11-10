729e01517e1902124e54f165e245293bde50bb69
Eirik Ola Aksnes <eoaksnes@gmail.com>
2023-11-10 14:26:07 +0100

    refactor: lazy import plugins

--- packages/dm-core-plugins/src/role_filter/RoleFilterPlugin.tsx
+++ packages/dm-core-plugins/src/role_filter/RoleFilterPlugin.tsx
@@ -5,6 +5,9 @@ import {
 } from '@development-framework/dm-core'
 import React, { useContext, useEffect, useState } from 'react'
 import { Banner, Icon } from '@equinor/eds-core-react'
+import { edit_text, save, thumbs_down } from '@equinor/eds-icons'
+
+Icon.add({ thumbs_down, save, edit_text })
 
 type FilteredView = {
   type: string
