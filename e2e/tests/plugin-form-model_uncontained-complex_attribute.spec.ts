167d25ba9df03ca75d51a27aa4a2af6054596d15
Roar Larsen <roarla@equinor.com>
2023-11-29 10:12:02 +0100

    fix: model uncontained test

--- e2e/tests/plugin-form-model_uncontained-complex_attribute.spec.ts
+++ e2e/tests/plugin-form-model_uncontained-complex_attribute.spec.ts
@@ -18,6 +18,7 @@ test('Model uncontained complex attribute', async ({ page }) => {
     await page.getByRole('button', { name: 'Edit' }).click()
     await expect(page.getByLabel('Name')).toHaveValue('TheBlackPearl')
     await page.getByRole('button', { name: 'Open in tab', exact: true }).click()
+    await expect(page.getByRole('tab', { name: 'captain' })).toBeVisible()
     await expect(page.getByRole('code')).toBeVisible()
     await page.getByRole('button', { name: 'Edit' }).nth(1).click()
     await expect(page.getByTestId('form-text-widget-name').nth(1)).toHaveValue(
@@ -27,39 +28,27 @@ test('Model uncontained complex attribute', async ({ page }) => {
       page.getByTestId('form-number-widget-Phone Number (optional)')
     ).toHaveValue('0')
     await page.getByLabel('Close captain').click()
+    await expect(page.getByRole('tab', { name: 'captain' })).not.toBeVisible()
   })
 
   await test.step('Update model uncontained', async () => {
     await page
       .getByRole('button', { name: 'Edit and save', exact: true })
       .click()
-    await page
-      .getByTestId('captain')
-      .getByRole('button', { name: 'DemoDataSource' })
-      .click()
-    await page
-      .getByTestId('captain')
-      .getByRole('button', { name: 'plugins' })
-      .click()
-    await page
-      .getByTestId('captain')
-      .getByRole('button', { name: 'form' })
-      .click()
-    await page
-      .getByTestId('captain')
-      .getByRole('button', { name: 'model_uncontained' })
-      .click()
-    await page
-      .getByTestId('captain')
-      .getByRole('button', { name: 'complex_attribute' })
-      .click()
-    await page
-      .getByTestId('captain')
-      .getByRole('button', { name: 'Barbossa' })
-      .click()
-    await page.getByRole('button', { name: 'Select', exact: true }).click()
-    await page.waitForTimeout(1000)
+
+    const dialog = page.getByRole('dialog')
+    await expect(dialog).toBeVisible()
+    await dialog.getByRole('button', { name: 'DemoDataSource' }).click()
+    await dialog.getByRole('button', { name: 'plugins' }).click()
+    await dialog.getByRole('button', { name: 'form' }).click()
+    await dialog.getByRole('button', { name: 'model_uncontained' }).click()
+    await dialog.getByRole('button', { name: 'complex_attribute' }).click()
+    await dialog.getByRole('button', { name: 'Barbossa' }).click()
+    await dialog.getByRole('button', { name: 'Select', exact: true }).click()
+    await expect(dialog).not.toBeVisible()
+
     await page.getByRole('button', { name: 'Open in tab', exact: true }).click()
+    await expect(page.getByRole('tab', { name: 'captain' })).toBeVisible()
     await expect(page.getByRole('code')).toBeVisible()
     await page.getByRole('button', { name: 'Edit' }).nth(1).click()
     await expect(page.getByTestId('form-text-widget-name').nth(1)).toHaveValue(
