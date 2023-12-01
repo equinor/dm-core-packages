576780d892173e1b7d990dfd0c9b2f6f901b06b4
Roar Larsen <roarla@equinor.com>
2023-12-01 10:11:32 +0100

    fix: remove trace

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'on-first-retry',
     video: 'retain-on-failure',
   },
 
