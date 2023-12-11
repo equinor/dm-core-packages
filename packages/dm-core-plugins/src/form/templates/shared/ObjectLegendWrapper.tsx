4fdf0b4e7f6b0fce4aee2053196ce74d483ceac1
Peter Bull Hove <peterbullhove@gmail.com>
2023-12-11 11:05:06 +0100

    refactor: rounded-[inherit]

--- packages/dm-core-plugins/src/form/templates/shared/ObjectLegendWrapper.tsx
+++ packages/dm-core-plugins/src/form/templates/shared/ObjectLegendWrapper.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from 'react'
 const ObjectLegendWrapper = ({ children }: PropsWithChildren) => {
   return (
     <legend
-      className={`flex h-10 justify-between bg-[#f7f7f7] items-center pr-2 rounded-sm`}
+      className={`flex h-10 justify-between bg-[#f7f7f7] items-center pr-2 rounded-[inherit]`}
     >
       {children}
     </legend>
