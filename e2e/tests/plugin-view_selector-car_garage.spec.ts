dd72f1f3c015cedf3735cdab9e2b2a34a2a47c95
Peter Bull Hove <peterbullhove@gmail.com>
2023-12-11 16:24:30 +0100

    fix: test

--- e2e/tests/plugin-view_selector-car_garage.spec.ts
+++ e2e/tests/plugin-view_selector-car_garage.spec.ts
@@ -54,7 +54,7 @@ test('View selector - car garage', async ({ page }) => {
   })
 
   await test.step('Add earlier owner', async () => {
-    await page.getByRole('button', { name: 'Add', exact: true }).click()
+    await page.getByLabel('Append primitive').click()
     await page.getByRole('textbox').last().fill(' Joanna')
     await page.getByRole('button', { name: 'Submit' }).click()
     await expect(page.getByRole('alert')).toHaveText(['Document updated'])
