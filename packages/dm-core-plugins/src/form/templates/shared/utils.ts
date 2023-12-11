95cf9f11f9c218f936b9126a9fec9bfa28e619ac
Peter Bull Hove <peterbullhove@gmail.com>
2023-12-11 13:19:23 +0100

    feat: for storage uncontained and arrays

---
+++ packages/dm-core-plugins/src/form/templates/shared/utils.ts
@@ -0,0 +1,42 @@
+import { TUiAttributeObject } from '../../types'
+
+export const ExpandViewConfig = (uiAttribute?: TUiAttributeObject) => {
+  return uiAttribute?.expandViewConfig
+    ? uiAttribute?.expandViewConfig
+    : {
+        type: 'ReferenceViewConfig',
+        recipe: uiAttribute?.uiRecipe,
+      }
+}
+
+export const OpenViewConfig = (
+  uiAttribute?: TUiAttributeObject,
+  namePath?: string
+) => {
+  return uiAttribute?.openViewConfig
+    ? uiAttribute?.openViewConfig
+    : {
+        type: 'ReferenceViewConfig',
+        scope: namePath,
+        recipe: uiAttribute?.uiRecipe,
+      }
+}
+
+export const InferCanOpenOrExpand = (
+  objectIsNotEmpty: boolean,
+  config: any,
+  uiAttribute?: TUiAttributeObject,
+  onOpen?: CallableFunction
+) => {
+  const canOpenInTab =
+    objectIsNotEmpty &&
+    onOpen &&
+    (uiAttribute?.functionality?.open ?? config.functionality.open)
+
+  const canExpand =
+    objectIsNotEmpty &&
+    !canOpenInTab &&
+    (uiAttribute?.functionality?.expand ?? config.functionality.expand)
+
+  return { canExpand, canOpenInTab }
+}
