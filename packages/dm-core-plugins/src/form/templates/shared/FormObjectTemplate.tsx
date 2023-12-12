3cf091e9ea3c2f7b6f4d3e775405daf55bf90c5c
Peter Bull Hove <peterbullhove@gmail.com>
2023-12-12 11:24:42 +0100

    fix: overflow scrolling issue

--- packages/dm-core-plugins/src/form/templates/shared/FormObjectTemplate.tsx
+++ packages/dm-core-plugins/src/form/templates/shared/FormObjectTemplate.tsx
@@ -26,7 +26,7 @@ const FormTemplateHeaderActions = ({ children }: PropsWithChildren) => {
 
 const FormTemplateContent = ({ children }: PropsWithChildren) => {
   return (
-    <div className='border-t p-2 border-[#6f6f6f] overflow-auto w-full'>
+    <div className='border-t p-2 border-[#6f6f6f] max-h-300 overflow-auto w-full'>
       {children}
     </div>
   )
