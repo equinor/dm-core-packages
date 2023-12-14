4d097a3f186571dc873e94e4abb49c87d00e4258
Peter Bull Hove <peterbullhove@gmail.com>
2023-12-14 08:07:53 +0100

    feat: start of data grid

--- packages/dm-core-plugins/src/form/widgets/DimensionalScalarWidget.tsx
+++ packages/dm-core-plugins/src/form/widgets/DimensionalScalarWidget.tsx
@@ -1,6 +1,11 @@
 import React, { useState, ChangeEvent } from 'react'
 import { TWidget } from '../types'
-import { TextField, Tooltip, Typography } from '@equinor/eds-core-react'
+import {
+  EdsProvider,
+  TextField,
+  Tooltip,
+  Typography,
+} from '@equinor/eds-core-react'
 import { NumberFieldWithoutArrows } from '../components/NumberFieldWithoutArrows'
 
 const DimensionalScalarWidget = ({
@@ -18,52 +23,62 @@ const DimensionalScalarWidget = ({
     : typeof entity?.value === 'number'
 
   const InputField = isNumber ? NumberFieldWithoutArrows : TextField
+
   return (
-    <div style={widgetConfig?.width ? { maxWidth: widgetConfig?.width } : {}}>
+    <div
+      className={`border border-equinor-green ${
+        widgetConfig?.inline ? 'flex justify-between' : ''
+      }
+    ${widgetConfig?.width ? `w-[${widgetConfig?.width}]` : ''}
+    `}
+    >
+      <Tooltip title={widgetConfig?.description || entity?.description}>
+        <Typography
+          style={{
+            fontWeight: 'bold',
+            paddingLeft: '8px',
+            whiteSpace: 'nowrap',
+          }}
+        >
+          {widgetConfig?.label || entity?.label}
+        </Typography>
+      </Tooltip>
       <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Tooltip title={widgetConfig?.description || entity?.description}>
-          <Typography
-            style={{
-              fontWeight: 'bold',
-              paddingLeft: '8px',
-            }}
-          >
-            {widgetConfig?.label || entity?.label}
-          </Typography>
-        </Tooltip>
-      </div>
-      <InputField
-        unit={widgetConfig?.unit || entity?.unit}
-        type={isNumber ? 'number' : undefined}
-        meta={widgetConfig?.meta || entity?.meta}
-        helperText={error}
-        variant={error ? 'error' : undefined}
-        style={
-          isDirty && !error
-            ? // @ts-ignore
-              { '--eds-input-background': '#85babf5e' }
-            : {}
+        className={
+          widgetConfig?.inputBoxWidth
+            ? `w-[${widgetConfig?.inputBoxWidth}}]`
+            : ''
         }
-        disabled={readOnly}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => {
-          setError(e.target.value === '' ? 'Value is required' : '')
-          let targetValue: string | number = e.target.value
-          if (isNumber)
-            targetValue = e.target.value === '' ? 0 : parseFloat(e.target.value)
+      >
+        <InputField
+          unit={widgetConfig?.unit || entity?.unit}
+          type={isNumber ? 'number' : undefined}
+          meta={widgetConfig?.meta || entity?.meta}
+          helperText={error}
+          variant={error ? 'error' : undefined}
+          style={
+            isDirty && !error
+              ? // @ts-ignore
+                { '--eds-input-background': '#85babf5e' }
+              : {}
+          }
+          disabled={readOnly}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setError(e.target.value === '' ? 'Value is required' : '')
+            let targetValue: string | number = e.target.value
+            if (isNumber)
+              targetValue =
+                e.target.value === '' ? 0 : parseFloat(e.target.value)
 
-          const changed = isPrimitive
-            ? targetValue
-            : { ...entity, value: targetValue }
+            const changed = isPrimitive
+              ? targetValue
+              : { ...entity, value: targetValue }
 
-          onChange(changed)
-        }}
-        defaultValue={isPrimitive ? entity : entity?.value ?? ''}
-      />
+            onChange(changed)
+          }}
+          defaultValue={isPrimitive ? entity : entity?.value ?? ''}
+        />
+      </div>
     </div>
   )
 }
