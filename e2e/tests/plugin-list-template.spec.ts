0e72ecfc030932fc4bc223d16745b3490c3e99a3
Roar Larsen <roarla@equinor.com>
2024-01-05 14:49:15 +0100

    test: add list items using template

---
+++ e2e/tests/plugin-list-template.spec.ts
@@ -0,0 +1,61 @@
+import { Page, expect, test } from '@playwright/test'
+
+test('Add list item using template', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+  await page.getByRole('button', { name: 'DemoDataSource' }).click()
+  await page.getByRole('button', { name: 'plugins' }).click()
+  await page.getByRole('button', { name: 'list' }).click()
+  await page.getByRole('button', { name: 'templates' }).click()
+  await page.getByRole('button', { name: 'file templates' }).click()
+  await page.getByRole('button', { name: 'edit' }).click()
+
+  await page.getByTestId('task_list').getByLabel('Open in tab').click()
+  await page.getByLabel('append-item').click()
+  await page.getByRole('menuitem', { name: 'Template1' }).click()
+  await page.getByTestId('SaveList').click()
+  await page.getByTestId('expandListItem-3').click()
+  await expect(page.getByTestId('form-text-widget-Task title:')).toHaveValue(
+    'unnamed_task'
+  )
+  await expect(
+    page.getByTestId('form-text-widget-Assigned to: (Optional)')
+  ).toHaveValue('None yet')
+  await expect(
+    page.getByTestId('form-text-area-widget-Task description: (Optional)')
+  ).toContainText('Add your description here.')
+  await page.getByTestId('expandListItem-3').click()
+
+  await page.getByLabel('append-item').click()
+  await page.getByRole('menuitem', { name: 'Template2' }).click()
+  await page.getByTestId('SaveList').click()
+  await page.getByTestId('expandListItem-4').click()
+  await page.getByTestId('form-text-widget-Task title:').fill('Fix bicycle')
+  await page
+    .getByTestId('form-text-widget-Assigned to: (Optional)')
+    .fill('Anna')
+  await page
+    .getByTestId('form-text-area-widget-Task description: (Optional)')
+    .fill('Patch tire and adjust brakes')
+  await page.getByRole('button', { name: 'Submit' }).click()
+  await expect(page.getByRole('alert')).toHaveText(['Document updated'])
+
+  await page.reload()
+  await page.getByRole('button', { name: 'DemoDataSource' }).click()
+  await page.getByRole('button', { name: 'plugins' }).click()
+  await page.getByRole('button', { name: 'list' }).click()
+  await page.getByRole('button', { name: 'templates' }).click()
+  await page.getByRole('button', { name: 'file templates' }).click()
+  await page.getByRole('button', { name: 'edit' }).click()
+  await page.getByTestId('task_list').getByLabel('Open in tab').click()
+  await expect(page.getByRole('tabpanel')).toContainText('Fix bicycle')
+  await page.getByTestId('expandListItem-4').click()
+  await expect(page.getByTestId('form-text-widget-Task title:')).toHaveValue(
+    'Fix bicycle'
+  )
+  await expect(
+    page.getByTestId('form-text-widget-Assigned to: (Optional)')
+  ).toHaveValue('Anna')
+  await expect(
+    page.getByTestId('form-text-area-widget-Task description: (Optional)')
+  ).toContainText('Patch tire and adjust brakes')
+})
