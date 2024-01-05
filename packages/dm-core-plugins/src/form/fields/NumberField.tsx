67d0e8929053dc62ed359b4294bd6060fb8e0e76
Christopher Collin Løkken <chcl@equinor.com>
2024-01-05 16:01:21 +0100

    fix: dont set empty required number to 0

--- packages/dm-core-plugins/src/form/fields/NumberField.tsx
+++ packages/dm-core-plugins/src/form/fields/NumberField.tsx
@@ -33,9 +33,7 @@ export const NumberField = (props: TField) => {
           <Widget
             {...props}
             readOnly={readOnly}
-            onChange={(event: unknown) => {
-              onChange(event !== null ? Number(event) : null)
-            }}
+            onChange={onChange}
             value={value ?? ''}
             id={namePath}
             label={
