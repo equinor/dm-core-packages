e73895388d98dca0fa49ff7c6d82acb2b2436643
Roar Larsen <roarla@equinor.com>
2024-01-09 10:40:20 +0100

    fix: update assertions

--- e2e/tests/plugin-form-model_uncontained-complex_attribute.spec.ts
+++ e2e/tests/plugin-form-model_uncontained-complex_attribute.spec.ts
@@ -49,11 +49,9 @@ test('Model uncontained complex attribute', async ({ page }) => {
     await expect(page.getByRole('tab', { name: 'captain' })).toBeVisible()
     await expect(page.getByRole('code')).toBeVisible()
     await page.getByRole('button', { name: 'Edit' }).nth(1).click()
-    await expect(page.getByTestId('form-text-widget-name').nth(1)).toHaveValue(
-      'Barbossa'
-    )
+    await expect(page.getByRole('textbox')).toHaveValue('Barbossa')
     await expect(
-      page.getByTestId('form-number-widget-Phone Number (optional)')
+      page.getByTestId('form-number-widget-Phone Number (Optional)')
     ).toHaveValue('12345678')
   })
 })
