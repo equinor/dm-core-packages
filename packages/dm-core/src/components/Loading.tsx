6706353fa493a276d04cb35c6d89fa225e41996b
Peter Bull Hove <peterbullhove@gmail.com>
2024-01-30 12:52:12 +0100

    fix: loading ui

--- packages/dm-core/src/components/Loading.tsx
+++ packages/dm-core/src/components/Loading.tsx
@@ -1,9 +1,18 @@
 import { CircularProgress } from '@equinor/eds-core-react'
+import { useEffect, useState } from 'react'
 
 export const Loading = () => {
+  const [visibility, setVisibility] = useState<'hidden' | 'visible'>('hidden')
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setVisibility('visible')
+    }, 300)
+    return () => clearInterval(interval)
+  }, [])
   return (
-    <div style={{ alignSelf: 'center', padding: '50px' }}>
-      <CircularProgress />
+    <div style={{ padding: '50px', visibility: visibility }}>
+      <CircularProgress size={24} />
     </div>
   )
 }
