ecdd0a4435bda48ba6885b0791a504fe9ac1bcfc
Peter Bull Hove <peterbullhove@gmail.com>
2024-03-19 14:35:38 +0100

    fix: reset job popup was misaligned like job logs flex issue

--- packages/dm-core-plugins/src/job/RemoveJobDialog.tsx
+++ packages/dm-core-plugins/src/job/RemoveJobDialog.tsx
@@ -10,24 +10,26 @@ type RemoveJobDialogProps = {
 export const RemoveJobDialog = (props: RemoveJobDialogProps) => {
   const { isOpen, onConfirm, close, title } = props
   return (
-    <Dialog open={isOpen}>
-      <Dialog.Header>
-        <Dialog.Title>
-          {title ?? 'Are you sure you want to stop the job?'}
-        </Dialog.Title>
-      </Dialog.Header>
-      <Dialog.Actions style={{ justifyContent: 'space-around' }}>
-        <Button
-          onClick={() => {
-            onConfirm()
-            close()
-          }}
-          color={'danger'}
-        >
-          Rerun
-        </Button>
-        <Button onClick={close}>Cancel</Button>
-      </Dialog.Actions>
-    </Dialog>
+    <div className='block'>
+      <Dialog open={isOpen}>
+        <Dialog.Header>
+          <Dialog.Title>
+            {title ?? 'Are you sure you want to stop the job?'}
+          </Dialog.Title>
+        </Dialog.Header>
+        <Dialog.Actions style={{ justifyContent: 'space-around' }}>
+          <Button
+            onClick={() => {
+              onConfirm()
+              close()
+            }}
+            color={'danger'}
+          >
+            Rerun
+          </Button>
+          <Button onClick={close}>Cancel</Button>
+        </Dialog.Actions>
+      </Dialog>
+    </div>
   )
 }
