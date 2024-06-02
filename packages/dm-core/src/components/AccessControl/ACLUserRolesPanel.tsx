253637d840bd96eb042140ec5cf71890395bdfc1
Andrea Vesterhus <andrea.vesterhus@bouvet.no>
2024-06-02 16:51:43 +0200

    refactor: ACL components tw removal + more cohesive design, remove unnecessary custom styled components

--- packages/dm-core/src/components/AccessControl/ACLUserRolesPanel.tsx
+++ packages/dm-core/src/components/AccessControl/ACLUserRolesPanel.tsx
@@ -1,7 +1,13 @@
-import { Button, EdsProvider, Icon, TextField } from '@equinor/eds-core-react'
+import {
+  Button,
+  EdsProvider,
+  Icon,
+  Table,
+  TextField,
+} from '@equinor/eds-core-react'
 import { close } from '@equinor/eds-icons'
 import React, { useState } from 'react'
-import styled from 'styled-components'
+import { Stack } from '../../layout'
 import { AccessControlList, AccessLevel } from '../../services'
 import { ACLSelect } from './ACLSelect'
 
@@ -10,22 +16,6 @@ interface IURPanelProps {
   handleChange: (data: Partial<AccessControlList>) => void
   aclKey: string
 }
-
-const TableWrapper = styled.div`
-  border: black 1px solid;
-  border-radius: 3px;
-  max-height: 40vh;
-  overflow: auto;
-  margin-bottom: 10px;
-`
-
-const GridContainer = styled.div<{ even: boolean }>`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  justify-items: center;
-  align-items: center;
-  background-color: ${(props) => (props.even ? '#f2f2f6' : '#d3d5de')};
-`
 
 export const ACLUserRolesPanel = ({
   roles,
@@ -37,8 +27,8 @@ export const ACLUserRolesPanel = ({
   const userTab = aclKey === 'users'
 
   return (
-    <>
-      <div className='flex justify-between my-2'>
+    <Stack spacing={1}>
+      <Stack direction='row' spacing={0.5} alignItems='flex-end'>
         <TextField
           id='new-role-user'
           style={{ width: '250px' }}
@@ -58,40 +48,49 @@ export const ACLUserRolesPanel = ({
         >
           Add +
         </Button>
-      </div>
-      <div className='flex justify-around'>
-        <div>{aclKey[0].toUpperCase() + aclKey.substring(1)}</div>
-        <div>Access Level</div>
-        <div></div>
-      </div>
-      <TableWrapper>
-        {Object.entries(roles).map(([role, access], index) => (
-          <GridContainer key={role} even={index % 2 === 0}>
-            {role}
-            <div style={{ gridColumn: '2 / span 3' }}>
-              <ACLSelect
-                value={access}
-                handleChange={(value: AccessLevel) => {
-                  roles[role] = value
-                  handleChange({ [aclKey]: roles })
-                }}
-              />
-            </div>
-            <EdsProvider density={'compact'}>
-              <Button
-                variant='ghost_icon'
-                onClick={() => {
-                  delete roles[role]
-                  handleChange({ [aclKey]: roles })
-                }}
-                aria-label='Remove role from acl'
-              >
-                <Icon size={16} data={close} />
-              </Button>
-            </EdsProvider>
-          </GridContainer>
-        ))}
-      </TableWrapper>
-    </>
+      </Stack>
+      <Table>
+        <Table.Head>
+          <Table.Row>
+            <Table.Cell>
+              {aclKey[0].toUpperCase() + aclKey.substring(1)}
+            </Table.Cell>
+            <Table.Cell>Access Level</Table.Cell>
+            <Table.Cell aria-label='Remove role' />
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {Object.entries(roles).map(([role, access], index) => (
+            <Table.Row key={role}>
+              <Table.Cell>{role}</Table.Cell>
+              <Table.Cell>
+                <ACLSelect
+                  value={access}
+                  handleChange={(value: AccessLevel) => {
+                    roles[role] = value
+                    handleChange({ [aclKey]: roles })
+                  }}
+                />
+              </Table.Cell>
+              <Table.Cell style={{ textAlign: 'right' }}>
+                <EdsProvider density={'compact'}>
+                  <Button
+                    color='secondary'
+                    variant='ghost_icon'
+                    onClick={() => {
+                      delete roles[role]
+                      handleChange({ [aclKey]: roles })
+                    }}
+                    aria-label='Remove role from acl'
+                  >
+                    <Icon size={16} data={close} />
+                  </Button>
+                </EdsProvider>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </Stack>
   )
 }
