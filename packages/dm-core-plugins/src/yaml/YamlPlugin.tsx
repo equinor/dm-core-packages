0cb5ce1f3a3fa174bf707eecf49be6f530cd78fd
Andrea Vesterhus <andrea.vesterhus@bouvet.no>
2024-06-13 16:33:34 +0200

    refactor: remove tw YamlPlugin

--- packages/dm-core-plugins/src/yaml/YamlPlugin.tsx
+++ packages/dm-core-plugins/src/yaml/YamlPlugin.tsx
@@ -11,6 +11,7 @@ import hljs from 'highlight.js'
 import { ChangeEvent, useMemo, useRef, useState } from 'react'
 import { toast } from 'react-toastify'
 import YAML from 'yaml'
+import { Stack } from '../common'
 import { ActionButton, ActionsWrapper, CodeContainer } from './styles'
 import { YamlPluginProps, defaultConfig } from './types'
 
@@ -66,76 +67,83 @@ export const YamlPlugin = (props: YamlPluginProps) => {
   if (error && !error.message) throw new Error(JSON.stringify(error, null, 2))
 
   return (
-    <div className='dm-plugin-padding flex flex-col w-full min-h-0 grow gap-1'>
-      {error && (
-        <div>
-          <ErrorGroup>{error.message}</ErrorGroup>
-        </div>
-      )}
-      <div className='flex min-h-0 grow bg-[#132634] overflow-auto relative'>
-        <div className='flex flex-col min-h-0 grow w-full'>
-          <CodeContainer
-            ref={textEditor}
-            contentEditable={isEditMode}
-            suppressContentEditableWarning={true}
-          >
-            {isEditMode ? (
-              <code>{showAsJSON ? asJSON : asYAML}</code>
-            ) : (
-              <code
-                dangerouslySetInnerHTML={{
-                  __html: hljs.highlight(showAsJSON ? asJSON : asYAML, {
-                    language: showAsJSON ? 'json' : 'yaml',
-                  })?.value,
-                }}
-              />
-            )}
-          </CodeContainer>
-        </div>
-        {isEditMode ? (
-          <ActionsWrapper>
-            <Tooltip title='Save'>
-              <ActionButton bg='green' onClick={saveChanges}>
-                <Icon data={save} />
-              </ActionButton>
-            </Tooltip>
-            <Tooltip title='Cancel'>
-              <ActionButton
-                onClick={() => {
-                  setIsEditMode(false)
-                  setError(null)
-                }}
-              >
-                <Icon data={close} />
-              </ActionButton>
-            </Tooltip>
-          </ActionsWrapper>
-        ) : (
-          <ActionsWrapper>
-            <Tooltip title='Change depth'>
-              <ActionButton
-                aria-controls='depth-popover'
-                aria-expanded={isDepthPopoverOpen}
-                aria-haspopup
-                bg='yellow'
-                id='depth-popover-anchor'
-                onClick={() => setIsDepthPopoverOpen(!isDepthPopoverOpen)}
-                ref={depthPopoverTrigger}
-              >
-                <Icon data={filter_alt} />
-                <span className='depth-indicator'>{depth}</span>
-              </ActionButton>
-            </Tooltip>
-            <Popover
-              anchorEl={depthPopoverTrigger.current}
-              id='depth-popover'
-              onClose={() => setIsDepthPopoverOpen(false)}
-              open={isDepthPopoverOpen}
-              placement='left'
-              trapFocus
+    <div className='dm-plugin-padding'>
+      <Stack fullWidth minHeight={0} grow={1} spacing={0.25}>
+        {error && (
+          <div>
+            <ErrorGroup>{error.message}</ErrorGroup>
+          </div>
+        )}
+        <Stack
+          direction='row'
+          grow={1}
+          minHeight={0}
+          scrollY
+          position='relative'
+          style={{ background: '#132634' }}
+        >
+          <Stack minHeight={0} grow={1} fullWidth>
+            <CodeContainer
+              ref={textEditor}
+              contentEditable={isEditMode}
+              suppressContentEditableWarning={true}
             >
-              <Popover.Content>
-                <div className='p-0.5'>
+              {isEditMode ? (
+                <code>{showAsJSON ? asJSON : asYAML}</code>
+              ) : (
+                <code
+                  dangerouslySetInnerHTML={{
+                    __html: hljs.highlight(showAsJSON ? asJSON : asYAML, {
+                      language: showAsJSON ? 'json' : 'yaml',
+                    })?.value,
+                  }}
+                />
+              )}
+            </CodeContainer>
+          </Stack>
+          {isEditMode ? (
+            <ActionsWrapper>
+              <Tooltip title='Save'>
+                <ActionButton bg='green' onClick={saveChanges}>
+                  <Icon data={save} />
+                </ActionButton>
+              </Tooltip>
+              <Tooltip title='Cancel'>
+                <ActionButton
+                  onClick={() => {
+                    setIsEditMode(false)
+                    setError(null)
+                  }}
+                >
+                  <Icon data={close} />
+                </ActionButton>
+              </Tooltip>
+            </ActionsWrapper>
+          ) : (
+            <ActionsWrapper>
+              <Tooltip title='Change depth'>
+                <ActionButton
+                  aria-controls='depth-popover'
+                  aria-expanded={isDepthPopoverOpen}
+                  aria-haspopup
+                  bg='yellow'
+                  id='depth-popover-anchor'
+                  onClick={() => setIsDepthPopoverOpen(!isDepthPopoverOpen)}
+                  ref={depthPopoverTrigger}
+                >
+                  <Icon data={filter_alt} />
+                  <span className='depth-indicator'>{depth}</span>
+                </ActionButton>
+              </Tooltip>
+              <Popover
+                anchorEl={depthPopoverTrigger.current}
+                id='depth-popover'
+                onClose={() => setIsDepthPopoverOpen(false)}
+                open={isDepthPopoverOpen}
+                placement='left'
+                trapFocus
+              >
+                <Popover.Content>
                   <TextField
                     id='depth_input'
                     label='Set depth'
@@ -143,34 +151,34 @@ export const YamlPlugin = (props: YamlPluginProps) => {
                     value={depth}
                     onChange={updateDepth}
                   />
-                </div>
-              </Popover.Content>
-            </Popover>
-            {config.editable && (
-              <Tooltip title='Edit'>
-                <ActionButton bg='green' onClick={() => setIsEditMode(true)}>
-                  <Icon data={edit} />
+                </Popover.Content>
+              </Popover>
+              {config.editable && (
+                <Tooltip title='Edit'>
+                  <ActionButton bg='green' onClick={() => setIsEditMode(true)}>
+                    <Icon data={edit} />
+                  </ActionButton>
+                </Tooltip>
+              )}
+              {config.languages?.length > 1 && (
+                <Tooltip title={`Switch to ${showAsJSON ? 'YAML' : 'JSON'}`}>
+                  <ActionButton onClick={() => setShowAsJSON(!showAsJSON)}>
+                    {showAsJSON ? 'YAML' : 'JSON'}
+                  </ActionButton>
+                </Tooltip>
+              )}
+              <Tooltip title='Copy'>
+                <ActionButton
+                  title={`Copy as ${showAsJSON ? 'JSON' : 'YAML'}`}
+                  onClick={copyToClipboard}
+                >
+                  <Icon data={copy} />
                 </ActionButton>
               </Tooltip>
-            )}
-            {config.languages?.length > 1 && (
-              <Tooltip title={`Switch to ${showAsJSON ? 'YAML' : 'JSON'}`}>
-                <ActionButton onClick={() => setShowAsJSON(!showAsJSON)}>
-                  {showAsJSON ? 'YAML' : 'JSON'}
-                </ActionButton>
-              </Tooltip>
-            )}
-            <Tooltip title='Copy'>
-              <ActionButton
-                title={`Copy as ${showAsJSON ? 'JSON' : 'YAML'}`}
-                onClick={copyToClipboard}
-              >
-                <Icon data={copy} />
-              </ActionButton>
-            </Tooltip>
-          </ActionsWrapper>
-        )}
-      </div>
+            </ActionsWrapper>
+          )}
+        </Stack>
+      </Stack>
     </div>
   )
 }
