f3d4176ca380df69b23307e9a8c421b9b8ddafd1
Andrea Vesterhus <andrea.vesterhus@bouvet.no>
2024-06-24 18:26:39 +0200

    refactor: responsive grid rewrite + remove react-grid-system package

---
+++ packages/dm-core-plugins/src/responsive_grid/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+import { TBreakpoint, TBreakpoints, breakpoints } from './types'
+
+const createSpacingStyles = (spacing: TBreakpoints) =>
+  Object.entries(spacing)
+    .filter((bp) => bp[0] !== 'type')
+    .map(
+      (bp) => `
+        @media (min-width: ${breakpoints[bp[0] as TBreakpoint]}px) {
+            gap: ${bp[1]}rem;
+        }
+    `
+    )
+    .join('')
+
+const createGridItemColumnSizes = (sizes: TBreakpoints) =>
+  Object.entries(sizes)
+    .filter((bp) => bp[0] !== 'type')
+    .map(
+      (bp) => `
+            @media (min-width: ${breakpoints[bp[0] as TBreakpoint]}px) {
+                grid-column: span ${bp[1]};
+            }
+        `
+    )
+    .join('')
+
+export const Grid = styled.div<{ spacing?: TBreakpoints }>`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  width: 100%;
+  gap: 1rem;
+  ${({ spacing }) => (spacing ? createSpacingStyles(spacing) : '')}
+`
+
+export const GridItem = styled.div<{ size: TBreakpoints }>`
+    grid-column: span 12;
+    ${({ size }) => createGridItemColumnSizes(size)}
+`
