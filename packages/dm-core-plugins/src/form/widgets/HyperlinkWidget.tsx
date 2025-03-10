b79889b2826c1f6bf9a43846a93cf2751fd076ac
awesthouse <avest@equinor.com>
2025-03-10 21:23:19 +0100

    feat: expanded config for hyperlinkwidget

--- packages/dm-core-plugins/src/form/widgets/HyperlinkWidget.tsx
+++ packages/dm-core-plugins/src/form/widgets/HyperlinkWidget.tsx
@@ -1,56 +1,46 @@
 import { Tooltip, Typography } from '@equinor/eds-core-react'
+import { type HTMLAttributeAnchorTarget, useMemo } from 'react'
 import type { TWidget } from '../types'
 
-function ensureProtocol(url: string): string {
-  if (!url.includes('://')) {
-    return 'https://' + url
-  }
-  return url
-}
-
-function isComplex(value: string | object) {
-  if (typeof value === 'object') return true
-  if (typeof value === 'string') return false
-  throw new Error('Invalid data type of value')
-}
-
-function isUrl(value: any): value is Url {
-  return (
-    value &&
-    typeof (value.value === 'string') &&
-    (typeof (value.label === 'string') || typeof value.label === 'undefined')
-  )
-}
-
-type Url = {
-  value: string
+type HyperlinkWidgetConfig = {
   label?: string
+  target?: HTMLAttributeAnchorTarget
+  download?: boolean
+  customDownloadFilename?: string
+}
+
+const defaultConfig = {
+  label: undefined,
+  target: '_self',
+  download: false,
+  customDownloadFilename: undefined,
 }
 
 const HyperlinkWidget = (props: TWidget) => {
   const { value, config } = props
+  const widgetConfig: HyperlinkWidgetConfig = { ...defaultConfig, ...config }
 
-  let url: string
-  let label: string
-  if (isComplex(value) && isUrl(value)) {
-    url = value.value
-    label = config?.label || value.label
-  } else {
-    url = value
-    label = config?.label
-  }
-  url = ensureProtocol(url)
+  const [url, label, rel] = useMemo(() => {
+    const url = typeof value === 'object' ? value?.value : value
+    const label = widgetConfig?.label || value.label || url
+    const rel = url.includes('http') ? 'noopener noreferrer' : ''
+    return [url, label, rel]
+  }, [value, widgetConfig])
 
   return (
     <Tooltip title={url}>
-      <a href={url} style={{ display: 'inline-block' }}>
-        <Typography
-          color='primary'
-          token={{ textDecoration: 'underline', fontWeight: 500 }}
-        >
-          {label ?? url}
-        </Typography>
-      </a>
+      <Typography
+        as='a'
+        variant='body_short_link'
+        style={{ display: 'inline-block', cursor: 'pointer' }}
+        token={{ textDecoration: 'underline', fontWeight: 500 }}
+        href={url}
+        target={widgetConfig.target}
+        rel={rel}
+        download={widgetConfig.customDownloadFilename || widgetConfig.download}
+      >
+        {label ?? url}
+      </Typography>
     </Tooltip>
   )
 }
