1a740f223706c0a3afed719e869ef382f804040d
Jakob Keller <jakel@equinor.com>
2026-08-04 10:33:06 +0200

    fix: Type-issue handled for attribute being undefined

--- packages/dm-core-plugins/src/form/components/Form.tsx
+++ packages/dm-core-plugins/src/form/components/Form.tsx
@@ -151,12 +151,12 @@ export const Form = (props: TFormProps) => {
       const attribute = blueprint?.attributes.find(
         (attribute: TAttribute) => attribute.name === key
       )
-      if (!attribute)
-        throw new Error(`Attribute with name ${key} not found in blueprint`)
-      const isComplexArray =
-        Array.isArray(obj[key]) && !isPrimitiveType(attribute.attributeType)
-      if (isComplexArray) {
-        toRemoveFromPayload.push(key)
+      if (attribute) {
+        const isComplexArray =
+          Array.isArray(obj[key]) && !isPrimitiveType(attribute.attributeType)
+        if (isComplexArray) {
+          toRemoveFromPayload.push(key)
+        }
       }
     }
     toRemoveFromPayload.forEach((key) => delete obj[key])
