9712e670304caf04b49ee96f3e2dbb53b1b475e8
Henrik August Burmann <henrik.burmann@bouvet.no>
2026-09-07 12:51:53 +0200

    fix: Listplugin new item button text

--- packages/dm-core-plugins/src/list/ListPlugin.tsx
+++ packages/dm-core-plugins/src/list/ListPlugin.tsx
@@ -57,7 +57,7 @@ type TListConfig = {
   defaultPaginationRowsPerPage?: number
   label?: string
   width?: string
-  newItemButtonText?: string
+  newItemButtonText?: string //Button text
 }
 const defaultConfig: TListConfig = {
   expanded: false,
